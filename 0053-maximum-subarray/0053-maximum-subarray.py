56fe3489a3b8789ff4e58e8c7279df0cb52cef62
davidpangsw <davidpangsw@hotmail.com>
2024-10-21 12:40:04 -0700

    Time: 56 ms (98.45%), Space: 31.4 MB (7.03%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,14 +1,18 @@
 class Solution:
+    def divideAndConquer(self, nums: List[int]) -> int:
+        pass
+
     def maxSubArray(self, nums: List[int]) -> int:
-        left = 0
         result = (s := nums[0])
         for right in range(1, len(nums)):
-            while s <= 0 and left < right:
-                s -= nums[left]
-                left += 1
-            
-            s += nums[right]
-            result = max(result, s)
+            if s <= 0:
+                s = nums[right]
+
+                # to prevent all items are negative
+                result = max(result, s)
+            else:
+                s += nums[right]
+                result = max(result, s)
 
         return result
         
