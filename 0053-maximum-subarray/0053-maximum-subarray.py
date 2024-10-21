b8b8fed3343c34fdbe4673e2fc54179e6c1d7204
davidpangsw <davidpangsw@hotmail.com>
2024-10-21 12:47:59 -0700

    Time: 55 ms (98.58%), Space: 31.3 MB (7.03%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -7,12 +7,9 @@ class Solution:
         for right in range(1, len(nums)):
             if s <= 0:
                 s = nums[right]
-
-                # to prevent all items are negative
-                result = max(result, s)
             else:
                 s += nums[right]
-                result = max(result, s)
+            result = max(result, s)
 
         return result
         
