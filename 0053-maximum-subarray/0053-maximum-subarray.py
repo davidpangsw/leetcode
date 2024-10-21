8c823807c792811d91950149c4bb6440bcb50ef2
davidpangsw <davidpangsw@hotmail.com>
2024-10-21 12:10:16 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,8 +1,7 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         left = 0
-        s = nums[0]
-        result = nums[0]
+        result = (s = nums[0])
         for right in range(1, len(nums)):
             while s <= 0 and left < right:
                 s -= nums[left]
