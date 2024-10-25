25486e9cf686973f3e828075cde473362eb92ffd
davidpangsw <davidpangsw@hotmail.com>
2024-10-24 20:30:57 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
@@ -0,0 +1,36 @@
+def insert(tree, path, start):
+    print(tree, path, start)
+    if "data" in tree:
+        return False # early stop, not mandatory
+    
+    if start == len(path):
+        tree["data"] = path
+        return True
+
+    ind = path.find('/', start + 1)
+    if ind == -1:
+        ind = len(path)
+    key = path[start:ind]
+    if key not in tree:
+        tree[key] = {}
+    
+    return insert(tree[key], path, ind)
+
+def collect(tree):
+    # print(tree)
+    if "data" in tree:
+        yield tree["data"]
+        return
+
+    for key in tree:
+        for x in collect(tree[key]):
+            yield x
+
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        tree = {}
+        for f in folder:
+            insert(tree, f, 0)
+        # print("Printing")
+        return [x for x in collect(tree)]
+        
