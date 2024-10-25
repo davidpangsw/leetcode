088ebe95a8136977adaf322e576fac6516bc2503
davidpangsw <davidpangsw@hotmail.com>
2024-10-25 16:44:05 -0700

    Time: 67 ms (97.02%), Space: 18.1 MB (5.03%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -10,8 +10,7 @@ class Solution:
 
         matchedCount = 0
 
-        queue = []
-        qStart = 0
+        queue = collections.deque()
 
         result = None # (startInc, length)
         for i, c in enumerate(s):
@@ -19,26 +18,28 @@ class Solution:
             if c not in counts:
                 continue
 
+            # enqueue
             queue.append((i, c))
 
+            # if JUST complete the count, inc the matchedCount
             counts[c] -= 1
             if counts[c] == 0:
                 matchedCount += 1
             
             
             # dequeue as much as possible
-            while qStart < len(queue):
-                _, frontC = queue[qStart]
+            while queue:
+                _, frontC = queue[0]
                 if counts[frontC] < 0:
                     # dequeue
                     counts[frontC] += 1
-                    qStart += 1
+                    queue.popleft()
                 else:
                     break
 
             # if valid window, try to update the result
             if matchedCount == len(counts):
-                frontI, _ = queue[qStart]
+                frontI, _ = queue[0]
                 backI, _ = queue[-1]
                 if result is None or backI - frontI + 1 < result[1]:
                     result = frontI, backI + 1 - frontI
