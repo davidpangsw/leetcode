23b378051ce390e13b6c3aded6dcf3c66d4da081
davidpangsw <davidpangsw@hotmail.com>
2024-10-24 21:13:08 -0700

    Time: 55 ms (83.79%), Space: 29.7 MB (82.93%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
@@ -1,31 +1,24 @@
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
-        result = []
-        tree = {}
+        result = set()
         for f in sorted(folder):
-            # print(tree, f)
+            # print(result, f)
             start = 0
-            cur = tree
             while start < len(f):
                 # print("    " + str(tree))
-                if "data" in cur:
-                    break
 
                 end = f.find('/', start+1)
                 if end == -1:
                     end = len(f)
+                    result.add(f[:end])
+                    break
 
-
-                key = f[start:end]
-                if key not in cur:
-                    cur[key] = {}
-                cur = cur[key]
+                key = f[:end]
+                if key in result:
+                    break
+                
 
                 start = end
 
-            if "data" not in cur:
-                cur["data"] = f
-                result.append(f)
-
-        return result
+        return list(result)
         
