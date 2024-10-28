b17dfda04b2654dd6091ba8fb73fab288138d677
davidpangsw <davidpangsw@hotmail.com>
2024-10-27 20:05:56 -0700

    Time: 45 ms (97.53%), Space: 19.2 MB (24.43%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
@@ -8,15 +8,16 @@ class Solution:
                 if matrix[i][j] == 1:
                     matrix[i][j] = min(matrix[i-1][j-1], matrix[i-1][j], matrix[i][j-1]) + 1
                 
-        total = 0
-        for i in range(0, m):
-            for j in range(0, n):
-                total += matrix[i][j]
+        # total = 0
+        return sum([sum(r) for r in matrix])
+        # for i in range(0, m):
+        #     for j in range(0, n):
+        #         total += matrix[i][j]
 
-                # print(rect[i][j], streak, total)
-            # print(rect)
-        # print()
-        # [print(row) for row in matrix]
-        return total
+        #         # print(rect[i][j], streak, total)
+        #     # print(rect)
+        # # print()
+        # # [print(row) for row in matrix]
+        # return total
                 
 
