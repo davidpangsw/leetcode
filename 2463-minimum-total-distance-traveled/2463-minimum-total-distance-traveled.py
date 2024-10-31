e8182d4667e09612508632559098d9c82931cd93
davidpangsw <davidpangsw@hotmail.com>
2024-10-31 02:52:32 -0700

    Time: 542 ms (84%), Space: 19.4 MB (62.58%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.py
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.py
@@ -1,80 +1,62 @@
+INT_MAX = 2 * 10**9 * 100
 class Solution:
     def minimumTotalDistance(self, robot: List[int], factory: List[List[int]]) -> int:
-        # from outside to inside
-        # can move robot or factory (but factory doesn't have to fulfil)
+        # figure out some basic rules first
+        # factories with limit=0 are meaningless
         # if a robot has chosen one direction, it can just take the nearest factory (no need to go further unless the factory is already full), including the one it already locates
-        # thus, outer robots have no choices
+        # outer robots have no choices
+
+        # sort the robots
         robot.sort()
 
-        # clear empty factories and sort
-        newFactory = []
-        for fpos, flimit in factory:
-            if flimit > 0:
-                newFactory.append((fpos, flimit))
-        factory = newFactory
+        # clear factories and sort
+        factory = [(fpos, flimit) for fpos, flimit in factory if flimit > 0]
         factory.sort()
         
-        # print(robot)
-        # print(factory)
-        
+        print(robot)
+        print(factory)
+
         @cache
         def dp(x, y):
             """
             Give the result starting from robot[x] and factory[y],
+            where factory[y] is empty
             ignoring the previous items
             
             x < R
             factory[y] must be new (not filled by any robot)
             """
-            # print(f"dp({x}, {y})")
-            result = 2**63-1
+            result = INT_MAX
 
             total = 0
             if y >= len(factory):
-                return 2**63-1
+                return INT_MAX
 
             fpos, flimit = factory[y]
             for i in range(x, len(robot)):
-
                 rpos = robot[i]
 
-                # if empty factory, step next
-                while True:
-                    if flimit > 0:
-                        break
-                    # print(f"factory[{y}] is full")
-                    y += 1
-                    if y >= len(factory):
-                        return 2**63-1
-                    fpos, flimit = factory[y]
-
-                # conditions that robot[i] must take factory[y]
-                # debug = f"(DEBUG: {rpos}, {(fpos, flimit)}"
-                # if y + 1 < len(factory):
-                    # debug += f", {factory[y + 1]})"
-                    
-                mustTake = False
-                if rpos <= fpos:
-                    # outer robot
-                    mustTake = True
-                elif y + 1 < len(factory):
-                    # inner, but closer to the factory[y]
-                    fpos1, flimit1 = factory[y + 1]
-                    if abs(fpos - rpos) <= abs(fpos1 - rpos):
-                        mustTake = True
-                
+                # conditions that robot[i] must take factory[y] :
+                # outer robot; or
+                # inner robot but closer to the factory[y]
+                mustTake = (rpos <= fpos) or (y + 1 < len(factory) and abs(fpos - rpos) <= abs(factory[y + 1][0] - rpos))
                 if not mustTake:
-                    # may try the alternative that it doesn't take the factory
-                    # then, that factory must be removed (impossible to get better)
-                    # print(f"robot[{i}] tries not to take factory[{y}] {debug}")
-                    alternative = total + dp(i, y + 1)
-                    # print(f"robot[{i}] tries not to take factory[{y}] done {debug}")
-                    result = min(result, alternative)
+                    # may try the optional alternative that it doesn't take the factory
+                    # in this case, that factory must be removed (impossible to get better result if we keep it)
+                    result = min(result, total + dp(i, y + 1))
                 
-                # take it anyway
-                # print(f"robot[{i}] takes factory[{y}] {debug}")
+                # take the factory
                 total += abs(fpos - rpos)
                 flimit -= 1
+
+                # if factory is full, use next one
+                if flimit == 0:
+                    print(i, y)
+                    y += 1
+                    if y < len(factory):
+                        fpos, flimit = factory[y]
+                    elif i + 1 < len(robot):
+                        return INT_MAX
             result = min(result, total)
             return result
         
