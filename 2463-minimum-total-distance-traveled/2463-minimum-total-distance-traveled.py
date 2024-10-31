a8a2738bf00b68f0c91ddbf0ab79c4358d371399
davidpangsw <davidpangsw@hotmail.com>
2024-10-31 04:19:45 -0700

    Time: 1037 ms (80%), Space: 17.3 MB (74.83%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.py
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.py
@@ -46,12 +46,13 @@ class Solution:
                     # if factory has no rooms
                     if flimit == 0:
                         j -= 1
-                        if j >= 0:
-                            fpos, flimit = factory[j]
-                        elif i > 0: # there are robots left, but no more factories
+                        if j < 0 and i > 0: # there are robots left, but no more factories
                             result = RESULT_MAX
                             total = RESULT_MAX
                             break
+                        if j >= 0:
+                            fpos, flimit = factory[j]
+                        
                 mem[I][J] = min(result, total)
                 # print(f"mem[{I}][{J}] = {mem[I][J]}")
         return mem[-1][-1]
