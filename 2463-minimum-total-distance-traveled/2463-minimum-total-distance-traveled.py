b1a5173a697b983d2ecc2c6d640c32f1cdb32607
davidpangsw <davidpangsw@hotmail.com>
2024-10-31 04:14:27 -0700

    Time: 1051 ms (80%), Space: 17.4 MB (74.83%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.py
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.py
@@ -32,13 +32,13 @@ class Solution:
                     rpos = robot[i]
 
                     # conditions that robot[i] must take factory[j] :
-                    # outer robot; or
-                    # inner robot but closer to the factory[j]
+                    # j == 0; or
+                    # closer to the factory[j] than to factory[j-1]
                     # if j > 0:
                     #     print(f"{rpos} {factory[j-1]} {factory[j]}")
                     # else:
                     #     print(f"{rpos} {factory[j]}")
-                    mustTake = (rpos >= fpos) or (j - 1 >= 0 and abs(fpos - rpos) <= abs(factory[j-1][0] - rpos))
+                    mustTake = (j == 0) or (abs(fpos - rpos) <= abs(factory[j-1][0] - rpos))
                     if not mustTake:
                         # may try the optional alternative that it doesn't take the factory
                         # in this case, that factory must be removed (impossible to get better result if we keep it)
@@ -63,65 +63,3 @@ class Solution:
                 mem[I][J] = min(result, total)
                 # print(f"mem[{I}][{J}] = {mem[I][J]}")
         return mem[-1][-1]
-
-
-
-    def minimumTotalDistanceOld(self, robot: List[int], factory: List[List[int]]) -> int:
-        # figure out some basic rules first
-        # factories with limit=0 are meaningless
-        # if a robot has chosen one direction, it can just take the nearest factory (no need to go further unless the factory is already full), including the one it already locates
-        # outer robots have no choices
-
-        # sort the robots
-        robot.sort()
-
-        # clear factories and sort
-        factory = [(fpos, flimit) for fpos, flimit in factory if flimit > 0]
-        factory.sort()
-        
-        # print(robot)
-        # print(factory)
-
-        @cache
-        def dp(x, y):
-            """
-            Return the result starting from robot[x] and factory[y],
-            where factory[y] is empty
-            ignoring the previous items
-            
-            x < R
-            factory[y] must be new (not filled by any robot)
-            """
-            result = RESULT_MAX
-
-            total = 0
-            if y >= len(factory):
-                return RESULT_MAX
-
-            fpos, flimit = factory[y]
-            for i in range(x, len(robot)):
-                rpos = robot[i]
-
-                # conditions that robot[i] must take factory[y] :
-                # outer robot; or
-                # inner robot but closer to the factory[y]
-                mustTake = (rpos <= fpos) or (y + 1 < len(factory) and abs(fpos - rpos) <= abs(factory[y + 1][0] - rpos))
-                if not mustTake:
-                    # may try the optional alternative that it doesn't take the factory
-                    # in this case, that factory must be removed (impossible to get better result if we keep it)
-                    result = min(result, total + dp(i, y + 1))
-                
-                # take the factory
-                total += abs(fpos - rpos)
-                flimit -= 1
-
-                # if factory is full, use next one
-                if flimit == 0:
-                    y += 1
-                    if y < len(factory):
-                        fpos, flimit = factory[y]
-                    elif i + 1 < len(robot):
-                        return RESULT_MAX
-            return min(result, total)
-        
-        return dp(0, 0)
