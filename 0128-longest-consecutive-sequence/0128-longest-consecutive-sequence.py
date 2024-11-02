28179d79ee64069d6e9388deffc88d4e5a1b2ed5
davidpangsw <davidpangsw@hotmail.com>
2024-11-02 13:28:41 -0700

    Time: 111 ms (45.15%), Space: 40 MB (6.19%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -3,27 +3,14 @@ class Solution:
         result = 0
         elemToLength = {}
         for x in nums:
-            # print("Adding ", x)
             if x in elemToLength:
                 continue
-            elif x+1 in elemToLength and x-1 in elemToLength:
-                # merge
-                newLength = elemToLength[x-1] + 1 + elemToLength[x+1]
-                elemToLength[x-elemToLength[x-1]] = newLength
-                elemToLength[x+elemToLength[x+1]] = newLength
-                elemToLength[x] = newLength
-                result = max(result, newLength)
-            elif x+1 in elemToLength:
-                newLength = 1 + elemToLength[x+1]
-                elemToLength[x-0] = newLength
-                elemToLength[x+elemToLength[x+1]] = newLength
-                result = max(result, newLength)
-            elif x-1 in elemToLength:
-                newLength = 1 + elemToLength[x-1]
-                elemToLength[x-elemToLength[x-1]] = newLength
-                elemToLength[x+0] = newLength
-                result = max(result, newLength)
-            else:
-                elemToLength[x] = 1
-                result = max(result, 1)
+
+            a = elemToLength.get(x-1, 0)
+            b = elemToLength.get(x+1, 0)
+            newLength = a + 1 + b
+            elemToLength[x-a] = newLength
+            elemToLength[x+b] = newLength
+            elemToLength[x] = newLength
+            result = max(result, newLength)
         return result
