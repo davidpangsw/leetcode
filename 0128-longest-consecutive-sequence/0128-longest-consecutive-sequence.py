51a6921bee094a733b3163047f9214305eb2fdda
davidpangsw <davidpangsw@hotmail.com>
2024-11-02 13:35:45 -0700

    Time: 79 ms (49.67%), Space: 40.9 MB (5.5%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -5,7 +5,7 @@ class Solution:
         for x in nums:
             a = elemToLength.get(x-1, 0)
             b = elemToLength.get(x+1, 0)
-            elemToLength[x-a] = a + 1 + b
-            elemToLength[x+b] = a + 1 + b
+            newLength = a + 1 + b
+            elemToLength[x-a] = newLength
+            elemToLength[x+b] = newLength
         return max(elemToLength.values()) if nums else 0
-        # return result
