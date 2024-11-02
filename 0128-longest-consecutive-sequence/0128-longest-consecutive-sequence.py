8d13c214ecb014d3754f737f52013011535ccc2c
davidpangsw <davidpangsw@hotmail.com>
2024-11-02 13:28:12 -0700

    Time: 123 ms (43.97%), Space: 40.1 MB (6.13%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,52 +1,29 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-
-        # result = 0
-        # groups = []
-        # elemToGroup = {}
-        # for x in nums:
-        #     print("Adding ", x)
-        #     if x in elemToGroup:
-        #         continue
-        #     elif x+1 in elemToGroup and x-1 in elemToGroup:
-        #         result = max(result, elemToGroup[x-1] + 1 + elemToGroup[x+1])
-        #     elif x+1 in elemToGroup:
-        #     elif x-1 in elemToGroup:
-        #     else:
-        # return result
-
         result = 0
-        groups = []
-        elemToGroup = {}
+        elemToLength = {}
         for x in nums:
             # print("Adding ", x)
-            if x in elemToGroup:
+            if x in elemToLength:
                 continue
-            elif x+1 in elemToGroup and x-1 in elemToGroup:
+            elif x+1 in elemToLength and x-1 in elemToLength:
                 # merge
-                # print("Merge...")
-                g1 = elemToGroup[groups[elemToGroup[x-1]][0]]
-                g2 = elemToGroup[groups[elemToGroup[x+1]][1]]
-                # print(x-1, x+1, groups[g1], groups[elemToGroup[g2]])
-
-                groups[g1][1] = groups[g2][1]
-                # groups[g2][0] = groups[g1][0]
-
-                groups[g2] = groups[g1] # key: change by reference!
-                
-                g = g1
-                # print(groups[elemToGroup[x-1]], groups[elemToGroup[x+1]])
-            elif x+1 in elemToGroup:
-                g = elemToGroup[x+1]
-                groups[g][0] = x
-            elif x-1 in elemToGroup:
-                g = elemToGroup[x-1]
-                groups[g][1] = x
+                newLength = elemToLength[x-1] + 1 + elemToLength[x+1]
+                elemToLength[x-elemToLength[x-1]] = newLength
+                elemToLength[x+elemToLength[x+1]] = newLength
+                elemToLength[x] = newLength
+                result = max(result, newLength)
+            elif x+1 in elemToLength:
+                newLength = 1 + elemToLength[x+1]
+                elemToLength[x-0] = newLength
+                elemToLength[x+elemToLength[x+1]] = newLength
+                result = max(result, newLength)
+            elif x-1 in elemToLength:
+                newLength = 1 + elemToLength[x-1]
+                elemToLength[x-elemToLength[x-1]] = newLength
+                elemToLength[x+0] = newLength
+                result = max(result, newLength)
             else:
-                g = len(groups)
-                groups.append([x, x])
-            elemToGroup[x] = g
-            result = max(result, groups[g][1] + 1 - groups[g][0])
-
-            # print(groups, elemToGroup)
+                elemToLength[x] = 1
+                result = max(result, 1)
         return result
