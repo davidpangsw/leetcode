b5f7fa821c437842888eaa5e354e80f6eba9f46d
davidpangsw <davidpangsw@hotmail.com>
2024-11-01 21:01:08 -0700

    Time: 3 ms (83.42%), Space: 16.8 MB (7.13%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -4,7 +4,7 @@ def isValid(items):
         if x == '.':
             continue
         if x in d:
-            # print("Not valid", items)
+            print("Not valid", items)
             return False
         d.add(x)
     
@@ -14,7 +14,9 @@ def col(board, j):
     for row in board:
         yield row[j]
 
-def grid(board, i, j):
+def grid(board, ind):
+    i = (ind // 3) * 3
+    j = (ind % 3) * 3
     for di in range(3):
         for dj in range(3):
             yield board[i+di][j+dj]
@@ -22,21 +24,13 @@ def grid(board, i, j):
 class Solution:
 
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        # rows
-        for row in board:
-            if not isValid(row):
+        for ind in range(9):
+            if not isValid(board[ind]):
                 return False
-        
-        # cols
-        for j in range(9):
-            if not isValid([x for x in col(board, j)]):
+            if not isValid(col(board, ind)):
                 return False
-
-        # grid
-        for i in range(0, 9, 3):
-            for j in range(0, 9, 3):
-                if not isValid([x for x in grid(board, i, j)]):
-                    return False
+            if not isValid(grid(board, ind)):
+                return False
 
         return True
                 
