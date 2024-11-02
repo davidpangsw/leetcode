fdc2e3b3485f2f6a225e723534a547b62825865e
davidpangsw <davidpangsw@hotmail.com>
2024-11-02 13:37:28 -0700

    Time: 89 ms (48.2%), Space: 41 MB (5.5%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -3,9 +3,12 @@ class Solution:
         nums = list(set(nums))
         elemToLength = {}
         for x in nums:
+            if x in elemToLength:
+                continue
             a = elemToLength.get(x-1, 0)
             b = elemToLength.get(x+1, 0)
             newLength = a + 1 + b
             elemToLength[x-a] = newLength
             elemToLength[x+b] = newLength
+            elemToLength[x] = newLength
         return max(elemToLength.values()) if nums else 0
