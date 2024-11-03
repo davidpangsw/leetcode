5fcaaf73e4d7eeefbe0a3ba67046c677a5a7a773
davidpangsw <davidpangsw@hotmail.com>
2024-11-03 01:38:59 -0800

    Time: 0 ms (100%), Space: 16.5 MB (40.35%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -1,4 +1,3 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        ind = (goal + goal).find(s)
-        return ind != -1 and len(s) <= len(goal)
+        return (goal + goal).find(s) != -1 and len(s) <= len(goal)
