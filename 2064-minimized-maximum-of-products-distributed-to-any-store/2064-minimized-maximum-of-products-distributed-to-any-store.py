0f5301a1746b5197001bcf8a78be6595a9033804
davidpangsw <davidpangsw@hotmail.com>
2024-11-16 13:22:42 -0800

    Time: 6156 ms (5%), Space: 35.8 MB (7.48%) - LeetHub

---
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minimizedMaximum(self, n: int, quantities: List[int]) -> int:
+        # maximize the minimum number of stores assigned to each product
+
+        # stores[i] = number of stores assigned to the i-th product type
+        # at least one for each type
+        m = len(quantities)
+        stores = [1] * m
+
+        # (-avg, q, s)
+        # average items per store, total quantities, stores assigned
+        data = [(-q, q, 1) for q in quantities]
+        heapq.heapify(data)
+
+        # for each store
+        for i in range(n - m):
+            # take the product type with maximum average items
+            # assign the store to that type
+            nAvg, q, s = heapq.heappop(data)
+            s += 1
+
+            # 11, 3 => 4
+            # 10, 3 => 4
+            # 9, 3 => 3
+            # 8, 3 => 3
+            avg = (q + (s-1)) // s
+            heapq.heappush(data, (-avg, q, s))
+
+        nAvg, q, s = heapq.heappop(data)
+        return -nAvg
