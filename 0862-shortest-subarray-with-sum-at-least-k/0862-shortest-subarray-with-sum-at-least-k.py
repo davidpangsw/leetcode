89648c475e2d65d4db25c7781a004059d6ec389e
davidpangsw <davidpangsw@hotmail.com>
2024-11-18 20:37:22 -0800

    Time: 193 ms (65.28%), Space: 33 MB (47.16%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -1,20 +1,32 @@
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
         n = len(nums)
+        # prefix sum
         sums = [0] * (n + 1)
         for i in range(n):
             sums[i+1] = sums[i] + nums[i]
         # sums[i] = sum(nums[:i])
         # sums[i] - nums[j] = sum[i:j]
-        print(sums) 
+        # print(sums) 
 
+        # "left" of the sliding windows
+        q = deque()
 
         result = n+1
         for i in range(n+1):
-            for diff in range(1, min(result, n+1-i)):
-                if sums[i+diff] - sums[i] >= k:
-                    result = diff
-                    break
+            while q and sums[i] - sums[q[0]] >= k:
+                result = min(result, i-q[0])
+                q.popleft()
+            
+            # larger prefix sum => cannot be the left of the subarray
+            while q and sums[q[-1]] >= sums[i]:
+                q.pop()
+
+            q.append(i)
+            # for diff in range(1, min(result, n+1-i)):
+            #     if sums[i+diff] - sums[i] >= k:
+            #         result = diff
+            #         break
         if result == n+1:
             return -1
         return result
