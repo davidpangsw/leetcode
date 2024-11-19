82e579dcb4277e5cd900cfc7cd7ce15cfe682723
davidpangsw <davidpangsw@hotmail.com>
2024-11-18 20:40:02 -0800

    Time: 359 ms (23.38%), Space: 35.3 MB (30.07%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -10,19 +10,26 @@ class Solution:
         # print(sums) 
 
         # "left" of the sliding windows
-        q = deque()
+        # use an array to implement the deque
+        # q = deque()
+        q = list(range(n+1))
+        q_from, q_to = 0, 0
+
 
         result = n+1
         for i in range(n+1):
-            while q and sums[i] - sums[q[0]] >= k:
-                result = min(result, i-q[0])
-                q.popleft()
+            while q_to - q_from > 0 and sums[i] - sums[q[q_from]] >= k:
+                result = min(result, i-q[q_from])
+                q_from += 1
             
             # larger prefix sum => cannot be the left of the subarray
-            while q and sums[q[-1]] >= sums[i]:
-                q.pop()
+            while q_to - q_from > 0 and sums[q[q_to-1]] >= sums[i]:
+                q_to -= 1
+            # use binary search as q is monotone
+            ind = bisect_left(q, sums[i], lo=q_from, hi=q_to)
 
-            q.append(i)
+            q[q_to] = i
+            q_to += 1
             # for diff in range(1, min(result, n+1-i)):
             #     if sums[i+diff] - sums[i] >= k:
             #         result = diff
