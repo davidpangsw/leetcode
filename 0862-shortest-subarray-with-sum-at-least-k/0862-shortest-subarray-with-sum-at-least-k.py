692e03540ecb9c9e9365b23030137dffd03b6ad2
davidpangsw <davidpangsw@hotmail.com>
2024-11-18 23:46:06 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -2,42 +2,39 @@
 Idea:
 1. Sliding window comes into mind first. Normal implementation is [left, right] with a "while True" loop
 2. However, we need to deal with the negative numbers:
-    - if [left, right] sums to a number < k, we cannot do right += 1. left += 1 is also possible.
+    - if [left, right] sums to a number < k, we cannot do right += 1, because left += 1 is also possible.
     - if we can ensure left += 1 is impossible, then our solution is still O(n)
 3. The problem is, for an index i, if there exists j > i such that [i, j] sums to negative, then left += 1 would possibly raise the window sum.
-4. So, everytime after we extend "right", we also do a check to eliminate these problematic i
-5. To do this, we use a deque to manage the possible "left"s
+4. So, everytime after we extend "right", we also do a check to eliminate these problematic indices ahead, such that left cannot be them
+5. To do this, we use a doubly linked queue to manage the possible "left"s
+    - array and deque are both okay
 """
 
+# "left" of the sliding windows
+# use an array to implement the deque
+# lefts = deque()
 lefts = [None] * 100001
+lefts_low, lefts_high = 0, 0
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
-        # "left" of the sliding windows
-        # use an array to implement the deque
-        # q = deque()
-        lefts[0] = (-1, 0) # [ind=0, curSum=0]
-        lefts_low, lefts_high = 0, 0
+        # lefts.push((-1, 0))
+        lefts[0] = (-1, 0) # [ind=-1, curSum=0]
 
         curSum = 0
         result = inf # math.inf
         for right in range(len(nums)):
             curSum += nums[right]
             if nums[right] > 0:
-                # can change to binary search
-                temp = None
+                # can change to binary search, but test cases don't favour this
+                ind = None
                 while lefts_low <= lefts_high and curSum - lefts[lefts_low][1] >= k:
-                    temp = lefts[lefts_low][0]
+                    # ind, _ = lefts.popleft()
+                    ind = lefts[lefts_low][0]
                     lefts_low += 1
-                if temp is not None:
-                    result = min(result, right-temp)
-                
-                # ind = bisect_right(lefts, curSum-k, lo=lefts_from, hi=lefts_to, key=lambda item: item[1])
-                # # print(lefts[lefts_from:lefts_to], curSum-k, ind)
-                # if ind > 0:
-                #     lefts_from = ind
-                #     result = min(result, right+1-lefts[lefts_from-1][0])
+                if ind is not None:
+                    result = min(result, right - ind) # ind can be -1
             else:
-                # can change to binary search
+                # can change to binary search, but test cases don't favour this
                 while lefts_low <= lefts_high and lefts[lefts_high][1] >= curSum:
                     lefts_high -= 1
             
