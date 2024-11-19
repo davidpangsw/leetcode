d437f8a5475ca4fa97232a28c6a1b60ce7da1ec2
davidpangsw <davidpangsw@hotmail.com>
2024-11-18 21:40:31 -0800

    Time: 353 ms (23.66%), Space: 35.3 MB (30%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -1,35 +1,46 @@
+"""
+Idea:
+1. Sliding window comes into mind first. Normal implementation is [left, right] with a "while True" loop
+2. However, we need to deal with the negative numbers:
+    - if [left, right] sums to a number < k, we cannot do right += 1. left += 1 is also possible.
+    - if we can ensure left += 1 is impossible, then our solution is still O(n)
+3. The problem is, for an index i, if there exists j > i such that [i, j] sums to negative, then left += 1 would possibly raise the window sum.
+
+4. So, everytime after we extend "right", we also do a check to eliminate the impossible "left"
+5.  To do this, we use a deque to manage the possible "left"
+"""
+
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
         n = len(nums)
+
         # prefix sum
         sums = [0] * (n + 1)
         for i in range(n):
             sums[i+1] = sums[i] + nums[i]
-        # sums[i] = sum(nums[:i])
-        # sums[i] - nums[j] = sum[i:j]
         # print(sums) 
 
         # "left" of the sliding windows
         # use an array to implement the deque
         # q = deque()
-        q = list(range(n+1))
-        q_from, q_to = 0, 0
-
+        lefts = list(range(n+1))
+        lefts_from, lefts_to = 0, 0
 
         result = n+1
         for i in range(n+1):
-            while q_to - q_from > 0 and sums[i] - sums[q[q_from]] >= k:
-                result = min(result, i-q[q_from])
-                q_from += 1
+            while lefts_to - lefts_from > 0 and sums[i] - sums[lefts[lefts_from]] >= k:
+                result = min(result, i-lefts[lefts_from])
+                lefts_from += 1
             
             # larger prefix sum => cannot be the left of the subarray
-            while q_to - q_from > 0 and sums[q[q_to-1]] >= sums[i]:
-                q_to -= 1
-            # use binary search as q is monotone
-            ind = bisect_left(q, sums[i], lo=q_from, hi=q_to)
+            while lefts_to - lefts_from > 0 and sums[lefts[lefts_to-1]] >= sums[i]:
+                lefts_to -= 1
 
-            q[q_to] = i
-            q_to += 1
+            # use binary search as lefts is monotone
+            ind = bisect_left(lefts, sums[i], lo=lefts_from, hi=lefts_to)
+
+            lefts[lefts_to] = i
+            lefts_to += 1
             # for diff in range(1, min(result, n+1-i)):
             #     if sums[i+diff] - sums[i] >= k:
             #         result = diff
