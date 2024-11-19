e9048edead5e49cdab60a0933fbf9378df556bd0
davidpangsw <davidpangsw@hotmail.com>
2024-11-18 21:48:23 -0800

    Time: 139 ms (99.83%), Space: 43 MB (5.35%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -1,43 +1,31 @@
-"""
-Idea:
-1. Sliding window comes into mind first. Normal implementation is [left, right] with a "while True" loop
-2. However, we need to deal with the negative numbers:
-    - if [left, right] sums to a number < k, we cannot do right += 1. left += 1 is also possible.
-    - if we can ensure left += 1 is impossible, then our solution is still O(n)
-3. The problem is, for an index i, if there exists j > i such that [i, j] sums to negative, then left += 1 would possibly raise the window sum.
-4. So, everytime after we extend "right", we also do a check to eliminate these problematic i
-5. To do this, we use a deque to manage the possible "left"s
-"""
-
+st=[None]*(100001)
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-
-        # "left" of the sliding windows
-        # use an array to implement the deque
-        # q = deque()
-        lefts = [None] * (n+1)
-        lefts_from, lefts_to = 0, 0
-
-        curSum = 0
-        result = n+1
-        for i in range(n+1):
-            while lefts_to - lefts_from > 0 and curSum - lefts[lefts_from][1] >= k:
-                result = min(result, i-lefts[lefts_from][0])
-                lefts_from += 1
+        n=len(nums)
+        ret=inf
+        # st=deque([(0,-1)])
+        st[0]=(0,-1)
+        cur=l=r=0
+        # for i,num in enumerate(nums,2):
+        for i in range(n):
+            cur+=nums[i]
+            if nums[i]>0:
+                tmp=None
+                # while st and cur-st[0][0]>=k:
+                #     tmp=st[0][1]
+                #     st.popleft()
+                while l<=r and cur-st[l][0]>=k:
+                    # if i-st[l][1]<ret:ret=i-st[l][1]
+                    tmp=st[l][1]
+                    l+=1
+                # print(tmp,i-tmp,ret)
+                if tmp is not None and i-tmp<ret:ret=i-tmp
+            else:
+                # while st and cur<=st[-1][0]:st.pop()
+                while r>=l and cur<=st[r][0]:r-=1
             
-            # larger prefix sum => cannot be the left of the subarray
-            while lefts_to - lefts_from > 0 and lefts[lefts_to-1][1] >= curSum:
-                lefts_to -= 1
-            lefts[lefts_to] = [i, curSum]
-            lefts_to += 1
-            if i < n:
-                curSum += nums[i]
+            # st.append((cur,i))
+            r+=1
+            st[r]=(cur,i)
 
-            # # use binary search as lefts is monotone
-            # ind = bisect_left(lefts, sums[i], lo=lefts_from, hi=lefts_to, key=lambda left: sums[left])
-            # lefts[ind] = i
-            # lefts_to = ind + 1
-        if result == n+1:
-            return -1
-        return result
+        return ret if ret!=inf else -1
