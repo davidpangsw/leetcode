8eb5936387d7e97fa13ee8afa2a61e7b25f33e0a
davidpangsw <davidpangsw@hotmail.com>
2024-11-21 01:52:17 -0800

    Time: 261 ms (97%), Space: 38.4 MB (71.35%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
@@ -1,7 +1,7 @@
 OBJECT = 2
 class Solution:
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
-        table = [[0 for _ in range(n)] for _ in range(m)]
+        table = [[1] * n for _ in range(m)]
         for x in chain(guards, walls):
             table[x[0]][x[1]] = OBJECT
         
@@ -12,22 +12,21 @@ class Solution:
             for j in range(col-1, -1, -1):
                 if table[row][j] is OBJECT:
                     break
-                table[row][j] = 1
+                table[row][j] = 0
 
             for j in range(col+1, n):
                 if table[row][j] is OBJECT:
                     break
-                table[row][j] = 1
+                table[row][j] = 0
 
             for i in range(row-1, -1, -1):
                 if table[i][col] is OBJECT:
                     break
-                table[i][col] = 1
+                table[i][col] = 0
 
             for i in range(row+1, m):
                 if table[i][col] is OBJECT:
                     break
-                table[i][col] = 1
+                table[i][col] = 0
 
-        # return sum([sum(row) for row in table]) - (len(guards) + len(walls)) * OBJECT
-        return sum([row.count(0) for row in table])
+        return sum([row.count(1) for row in table])
