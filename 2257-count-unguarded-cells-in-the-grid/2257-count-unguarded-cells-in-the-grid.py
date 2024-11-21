8fe8e168bb03264e6c1976042e0a50b131c8400a
davidpangsw <davidpangsw@hotmail.com>
2024-11-21 00:53:53 -0800

    Time: 616 ms (24%), Space: 38 MB (94.15%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
@@ -1,10 +1,10 @@
+GUARD = "G"
+WALL = "W"
 class Solution:
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
         # bitwise to make it faster? still O(m, n)
         result = 0
         table = [[True for _ in range(n)] for _ in range(m)]
-        GUARD = "G"
-        WALL = "W"
         for x in guards:
             table[x[0]][x[1]] = GUARD
         for x in walls:
@@ -48,38 +48,3 @@ class Solution:
                         result += 1
         # print(table)
         return result
-
-        # rows = [[] for _ in range(m)]
-        # cols = [[] for _ in range(n)]
-        # for x in guards:
-        #     i, j = x[0], x[1]
-        #     rows[i].append(j, GUARD)
-        #     cols[j].append(i, GUARD)
-        #     table[i][j] = GUARD
-        # for x in walls:
-        #     i, j = x[0], x[1]
-        #     rows[i].append(j, WALL)
-        #     cols[j].append(i, WALL)
-        #     table[i][j] = WALL
-
-        # for i in m:
-        #     rows[i].sort()
-        # for j in range(n):
-        #     cols[j].sort()
-
-        # # given i, j, 
-        # # want: closest forward guard/wall in the same row
-        # result = 0
-        # prevObj = EMPTY
-        # curObj = EMPTY
-        # for i in range(m):
-        #     for j in range(n):
-        #         curObj = table[i][j]
-        #         if (curObj == EMPTY and 
-        #             (prevObj == WALL and nextObj == WALL) and
-        #             (prevColObj == WALL and nextColObj == WALL)
-        #             ):
-        #             result += 1
-        
-        # return result
-        
