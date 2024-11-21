186dff99ee287fd2daa1917567694522fe915363
davidpangsw <davidpangsw@hotmail.com>
2024-11-21 01:30:03 -0800

    Time: 406 ms (52%), Space: 37.9 MB (99.42%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
@@ -1,50 +1,41 @@
-GUARD = 11
-WALL = 12
+GUARD = 2
+WALL = 3
+
 class Solution:
+
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
         # bitwise to make it faster? still O(m, n)
         result = 0
-        table = [[True for _ in range(n)] for _ in range(m)]
+        table = [[1 for _ in range(n)] for _ in range(m)]
         for x in guards:
             table[x[0]][x[1]] = GUARD
         for x in walls:
             table[x[0]][x[1]] = WALL
+        
+        for x in guards:
+            row, col = x[0], x[1]
+            for j in range(col-1, -1, -1):
+                i, j = row, j
+                if table[i][j] in [WALL, GUARD]:
+                    break
+                table[i][j] = 0
 
-        for i in range(m):
-            curObj = None
-            for j in range(n):
-                if table[i][j] in [GUARD, WALL]:
-                    curObj = table[i][j]
-                else:
-                    if curObj is GUARD:
-                        table[i][j] = False
+            for j in range(col+1, n):
+                i, j = row, j
+                if table[i][j] in [WALL, GUARD]:
+                    break
+                table[i][j] = 0
 
-            curObj = None
-            for j in range(n-1, -1, -1):
-                if table[i][j] in [GUARD, WALL]:
-                    curObj = table[i][j]
-                else:
-                    if curObj is GUARD:
-                        table[i][j] = False
+            for i in range(row-1, -1, -1):
+                i, j = i, col
+                if table[i][j] in [WALL, GUARD]:
+                    break
+                table[i][j] = 0
 
-        for j in range(n):
-            curObj = None
-            for i in range(m):
-                if table[i][j] in [GUARD, WALL]:
-                    curObj = table[i][j]
-                else:
-                    if curObj is GUARD:
-                        table[i][j] = False
-
-            curObj = None
-            for i in range(m-1, -1, -1):
-                if table[i][j] in [GUARD, WALL]:
-                    curObj = table[i][j]
-                else:
-                    if curObj is GUARD:
-                        table[i][j] = False
-
-                    if table[i][j] is True:
-                        result += 1
+            for i in range(row+1, m):
+                i, j = i, col
+                if table[i][j] in [WALL, GUARD]:
+                    break
+                table[i][j] = 0
         # print(table)
-        return result
+        return sum([sum(row) for row in table]) - len(guards) * GUARD - len(walls) * WALL
