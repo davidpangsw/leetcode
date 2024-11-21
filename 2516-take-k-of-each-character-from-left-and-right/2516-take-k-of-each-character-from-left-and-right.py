7e0d4774c677f0bf9b82fb7dbe7819a627541480
davidpangsw <davidpangsw@hotmail.com>
2024-11-20 22:50:21 -0800

    Time: 187 ms (79.23%), Space: 17.1 MB (79.65%) - LeetHub

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.py
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.py
@@ -1,10 +1,6 @@
 class Solution:
     def takeCharacters(self, s: str, k: int) -> int:
-        counts = {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-        }
+        counts = { "a": 0, "b": 0, "c": 0 }
         right = None
         for i, c in enumerate(s):
             counts[c] += 1
@@ -15,15 +11,15 @@ class Solution:
             return -1
         
         result = right
-        right -= 1
-        counts[s[right]] -= 1
+        # right -= 1
+        # counts[s[right]] -= 1
 
         n = len(s)
         left = n
         while right >= 0 and left > 0:
             # print(right, left)
             if counts["a"] >= k and counts["b"] >= k and counts["c"] >= k:
-                result = min(result, right+n-left)
+                result = min(result, right + (n - left))
                 right -= 1
                 counts[s[right]] -= 1 # in python, it is not an index error if right < 0 (doesn't matter here)
             else:
