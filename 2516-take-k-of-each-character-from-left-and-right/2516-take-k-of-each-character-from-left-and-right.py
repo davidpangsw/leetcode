9c8ce388018510739d7f8e6fe388b937b20c63ac
davidpangsw <davidpangsw@hotmail.com>
2024-11-20 22:51:30 -0800

    Time: 215 ms (60.87%), Space: 17.1 MB (95.64%) - LeetHub

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.py
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.py
@@ -1,5 +1,6 @@
 class Solution:
     def takeCharacters(self, s: str, k: int) -> int:
+        n = len(s)
         counts = { "a": 0, "b": 0, "c": 0 }
         for i, c in enumerate(s):
             counts[c] += 1
@@ -11,7 +12,6 @@ class Solution:
         right -= 1
         counts[s[right]] -= 1
 
-        n = len(s)
         left = n
         while right >= 0 and left > 0:
             # print(right, left)
