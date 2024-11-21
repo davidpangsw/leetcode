d10995ea76169e245efd9b0c6b302e29e21d92eb
davidpangsw <davidpangsw@hotmail.com>
2024-11-20 22:58:17 -0800

    Time: 186 ms (79.23%), Space: 17.1 MB (79.65%) - LeetHub

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.py
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.py
@@ -1,27 +1,24 @@
 class Solution:
     def takeCharacters(self, s: str, k: int) -> int:
         n = len(s)
-        counts = { "a": 0, "b": 0, "c": 0 }
+        counts = [0, 0, 0]
         for c in s:
-            counts[c] += 1
-        if not (counts["a"] >= k and counts["b"] >= k and counts["c"] >= k):
+            counts[ord(c) - ord("a")] += 1
+        if not (counts[0] >= k and counts[1] >= k and counts[2] >= k):
             return -1
         right = n
         
-        result = right
-        right -= 1
-        counts[s[right]] -= 1
-
+        result = n
         left = n
         while right >= 0 and left > 0:
             # print(right, left)
-            if counts["a"] >= k and counts["b"] >= k and counts["c"] >= k:
+            if counts[0] >= k and counts[1] >= k and counts[2] >= k:
                 result = min(result, right + (n - left))
                 right -= 1
-                counts[s[right]] -= 1 # in python, it is not an index error if right < 0 (doesn't matter here)
+                counts[ord(s[right]) - ord("a")] -= 1 # in python, it is not an index error if right < 0 (doesn't matter here)
             else:
                 left -= 1
-                counts[s[left]] += 1
+                counts[ord(s[left]) - ord("a")] += 1
         return result
 
 
