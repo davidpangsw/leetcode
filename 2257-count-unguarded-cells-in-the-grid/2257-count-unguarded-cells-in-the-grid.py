8ef6841f30e99bbc20f933c2c863a520ed44c06e
davidpangsw <davidpangsw@hotmail.com>
2024-11-21 00:58:40 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
@@ -4,46 +4,47 @@ class Solution:
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
         # bitwise to make it faster? still O(m, n)
         result = 0
-        table = [[True for _ in range(n)] for _ in range(m)]
+        # table = [[True for _ in range(n)] for _ in range(m)]
         for x in guards:
             table[x[0]][x[1]] = GUARD
         for x in walls:
             table[x[0]][x[1]] = WALL
 
         for i in range(m):
-            cur1, cur2 = None, None
+            curObj = None
             for j in range(n):
                 if table[i][j] in [GUARD, WALL]:
-                    cur1 = table[i][j]
+                    curObj = table[i][j]
                 else:
-                    if cur1 is GUARD:
+                    if curObj is GUARD:
                         table[i][j] = False
 
-                if table[i][n-1-j] in [GUARD, WALL]:
-                    cur2 = table[i][n-1-j]
+            curObj = None
+            for j in range(n-1, -1, -1):
+                if table[i][j] in [GUARD, WALL]:
+                    curObj = table[i][j]
                 else:
-                    if cur2 is GUARD:
-                        table[i][n-1-j] = False
+                    if curObj is GUARD:
+                        table[i][j] = False
 
         for j in range(n):
-            cur1, cur2 = None, None
+            curObj = None
             for i in range(m):
                 if table[i][j] in [GUARD, WALL]:
-                    cur1 = table[i][j]
+                    curObj = table[i][j]
                 else:
-                    if cur1 is GUARD:
+                    if curObj is GUARD:
                         table[i][j] = False
 
-                if table[m-1-i][j] in [GUARD, WALL]:
-                    cur2 = table[m-1-i][j]
+            curObj = None
+            for i in range(m-1, -1, -1):
+                if table[i][j] in [GUARD, WALL]:
+                    curObj = table[i][j]
                 else:
-                    if cur2 is GUARD:
-                        table[m-1-i][j] = False
-        
-        for i in range(m):
-            for j in range(n):
-                if table[i][j] is True:
-                    result += 1
-        
+                    if curObj is GUARD:
+                        table[i][j] = False
+
+                    if table[i][j] is True:
+                        result += 1
         # print(table)
         return result
