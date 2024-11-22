5699804de3b892643545ca451736d5e83581cf25
davidpangsw <davidpangsw@hotmail.com>
2024-11-21 21:07:16 -0800

    Time: 17 ms (100%), Space: 19.1 MB (64.32%) - LeetHub

--- 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.py
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.py
@@ -1,11 +1,8 @@
-"""
-group up same pattern, count, find the size of the biggest group
-Two rows are of same pattern if like this:
-    10011
-    01100
-
-Then after some flips
-"""
 class Solution:
     def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
-        return max(Counter(hash((1-bit for bit in row)) if row[0] == 1 else hash(tuple(row)) for row in matrix).values())
+        table = defaultdict(int)
+        for row in matrix:
+            table[hash(tuple([1-bit for bit in row])) if row[0] == 1 else hash(tuple(row))] += 1
+        return max(table.values())
+        
+
