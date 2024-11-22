1757f4d4092f6759ee51e6ec4e4f6d5e0bd1b9b9
davidpangsw <davidpangsw@hotmail.com>
2024-11-21 20:52:41 -0800

    Time: 47 ms (82.41%), Space: 19.6 MB (32.16%) - LeetHub

--- 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.py
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.py
@@ -1,30 +1,21 @@
 class Solution:
     def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
         m, n = len(matrix), len(matrix[0])
-        [print(row) for row in matrix]
+        # [print(row) for row in matrix]
         result = 0
-        for prev_good in [True, False]:
-            queue = deque(range(m))
-            for j in range(1, n):
-                q_good = deque()
-                q_bad = deque()
-                while queue:
-                    i = queue.popleft()
-                    if (matrix[i][j] == matrix[i][j-1]) == prev_good:
-                        q_good.append(i)
-                    else:
-                        q_bad.append(i)
+        table = defaultdict(int)
+        for row in matrix:
+            if row[0] == 0:
+                # hashValue = sum([str(bit) for bit in row])                s
+                key = str(row)
+            else:
+                key = str([1-bit for bit in row])
+            # print(key)
+            table[key] += 1
 
-                # print(q_good, q_bad)
-                if len(q_good) >= len(q_bad):
-                    # print(j, "good")
-                    queue = q_good
-                    prev_good = True
-                else:
-                    # print(j, "flip")
-                    queue = q_bad
-                    prev_good = False
-            result = max(result, len(queue))
-        return result
+
+
+
+        return max(table.values())
         
 
