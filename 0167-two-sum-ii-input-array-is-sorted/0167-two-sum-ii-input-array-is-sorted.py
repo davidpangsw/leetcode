c06d4d4358a34b6f7575233a4bd89e094bdc16c3
davidpangsw <davidpangsw@hotmail.com>
2024-11-23 06:33:35 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,5 +1,29 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers)-1
+        while True:
+            # print(left, right)
+
+            # fix left, find right
+            x = numbers[left]
+            y = target - x
+            newRight = bisect_left(numbers, y, lo=left+1, hi=right)
+            if numbers[newRight] == y:
+                return left+1, newRight+1
+            right = newRight
+
+            # fix right, find left
+            x = numbers[right]
+            y = target - x
+            newLeft = bisect_left(numbers, y, lo=left, hi=right-1)
+            if numbers[newLeft] == y:
+                return newLeft+1, right+1
+            left = newLeft
+            # print(left, right)
+            
+
+    
+    def twoSumStandard(self, numbers: List[int], target: int) -> List[int]:
         prev_x = None
         for i, x in enumerate(numbers):
             if x == prev_x:
