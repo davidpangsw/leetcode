eba95e627db6f852dc02f3e2215c91b4ee63697c
davidpangsw <davidpangsw@hotmail.com>
2024-11-23 05:46:59 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -6,11 +6,11 @@ class Solution:
         stack = []
         for i, x in enumerate(heights):
             while stack:
-                j, left = stack[-1]
+                j = stack[-1]
                 if x < heights[j]:
                     # i would be the right boundary that heights[j] can extend
+                    heights[j] = (i - stack[-1][1] - 1) * heights[j] # reuse heights to store the results
                     stack.pop()
-                    heights[j] = (i - left - 1) * heights[j] # reuse heights to store the results
                 elif x > heights[j]:
                     # the top item would be the left boundary that heights[i] can extend
                     stack.append([i, j])
