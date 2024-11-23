1e4f03eff4453ee6f57c6612d2aa16ee55b54024
davidpangsw <davidpangsw@hotmail.com>
2024-11-23 05:52:21 -0800

    Time: 99 ms (98.74%), Space: 28.7 MB (87.68%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -2,14 +2,16 @@ class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         heights.append(-1)
 
-        # stores (index, left boundary)
+        # stores (i, left boundary (exclusive) that heights[i] can extend)
         stack = []
         for i, x in enumerate(heights):
             while stack:
+                # j, left = stack[-1]
                 j = stack[-1][0]
                 if x < heights[j]:
                     # i would be the right boundary that heights[j] can extend
-                    heights[j] = (i - stack[-1][1] - 1) * heights[j] # reuse heights to store the results
+                    # results[j] = (i - stack[-1][1] - 1) * heights[j] # reuse heights to store the results
+                    heights[j] *= (i - stack[-1][1] - 1) # reuse heights to store the results
                     stack.pop()
                 elif x > heights[j]:
                     # j would be the left boundary that heights[i] can extend
