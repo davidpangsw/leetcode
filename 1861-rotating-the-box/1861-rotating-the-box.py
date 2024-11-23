32f843fc892c6cf2bd1d97e28659eb6059342815
davidpangsw <davidpangsw@hotmail.com>
2024-11-22 19:54:41 -0800

    Time: 1880 ms (73.73%), Space: 29.1 MB (43.28%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.py
+++ 1861-rotating-the-box/1861-rotating-the-box.py
@@ -4,7 +4,7 @@ EMPTY = "."
 class Solution:
     def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
         m, n = len(box), len(box[0])
-        result = [[EMPTY for _ in range(m)] for _ in range(n)]
+        result = [[EMPTY] * m for _ in range(n)]
         for i in range(m):
             stones = 0
             for j in range(n):
