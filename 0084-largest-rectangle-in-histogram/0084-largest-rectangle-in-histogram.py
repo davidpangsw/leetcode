b313dcfa482caacabffdc55e153951433137032b
davidpangsw <davidpangsw@hotmail.com>
2024-11-23 04:48:46 -0800

    Time: 192 ms (20.93%), Space: 34.2 MB (28.44%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -4,17 +4,15 @@ class Solution:
         lefts = [-1] * n
         rights = [n] * n
 
-        stack = []
+        st1, st2 = [], []
         for i in range(n):
-            while stack and heights[i] < heights[stack[-1]]:
-                rights[stack.pop()] = i
-            stack.append(i)
-
-        stack = []
-        for i in range(n):
-            while stack and heights[n-1-i] < heights[stack[-1]]:
-                lefts[stack.pop()] = n-1-i
-            stack.append(n-1-i)
+            while st1 and heights[i] < heights[st1[-1]]:
+                rights[st1.pop()] = i
+            st1.append(i)
+            
+            while st2 and heights[n-1-i] < heights[st2[-1]]:
+                lefts[st2.pop()] = n-1-i
+            st2.append(n-1-i)
         # results = [0] * n
         # return max(results)
 
