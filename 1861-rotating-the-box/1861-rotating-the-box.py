226d82c917ca93147c8fb35d1650f766f7fdf8d6
davidpangsw <davidpangsw@hotmail.com>
2024-11-22 19:57:56 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.py
+++ 1861-rotating-the-box/1861-rotating-the-box.py
@@ -8,14 +8,11 @@ class Solution:
         for i in range(m):
             bottom = n-1
             for j in range(n-1, -1, -1):
-                x = box[i][j]
-                if x == STONE:
-                    result[bottom][m-1-i] = STONE
-                    bottom -= 1
-                elif x == OBSTACLE:
-                    result[j][m-1-i] = OBSTACLE
-                    bottom = j - 1
-                else:
-                    # empty
-                    pass
+                match box[i][j]:
+                    case str(STONE):
+                        result[bottom][m-1-i] = STONE
+                        bottom -= 1
+                    case str(OBSTACLE):
+                        result[j][m-1-i] = OBSTACLE
+                        bottom = j - 1
         return result
