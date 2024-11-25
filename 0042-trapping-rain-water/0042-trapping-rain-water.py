91f45316c306c4514d0f4a694711cd7af07c307e
davidpangsw <davidpangsw@hotmail.com>
2024-11-24 16:23:46 -0800

    Time: 27 ms (31.63%), Space: 18.2 MB (98.32%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -0,0 +1,27 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        total = 0
+        curLevel = 0
+        left, right = 0, n-1
+
+        while left < right:
+            h1, h2 = height[left], height[right]
+
+            # if both dirts are high, change the curLevel, include more water
+            if min(h1, h2) > curLevel:
+                newLevel = min(h1, h2)
+                total += (right - left - 1) * (newLevel - curLevel)
+            else:
+                newLevel = curLevel
+            # remove the water occupied by dirt
+            # move the pointer
+            if h1 <= h2:
+                total -= min(height[left], curLevel)
+                left += 1
+            else:
+                total -= min(height[right], curLevel)
+                right -= 1
+
+            curLevel = newLevel
+        return total
