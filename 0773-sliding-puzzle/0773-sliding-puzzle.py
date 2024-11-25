6ebed5ff5a12ff7a4f5821cd744a27ab0355e841
davidpangsw <davidpangsw@hotmail.com>
2024-11-25 00:33:01 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0773-sliding-puzzle/0773-sliding-puzzle.py
+++ 0773-sliding-puzzle/0773-sliding-puzzle.py
@@ -11,33 +11,39 @@
 #         newBoard.append([x for x in row])
 #     return newBoard
 
+canMoveTo = [
+    [1, 3], [0, 2, 4], [1, 5],
+    [0, 4], [1, 3, 5], [2, 4],
+]
+
+COMPLETED = "123450"
 class Solution:
-    def slidingPuzzle(self, target: List[List[int]]) -> int:
-        # use string to represent a board
-        initial = "123450"
-        targetH = "".join(str(x) for row in target for x in row)
-        if initial == targetH:
-            return 0
-        
-        # check solvable
+    def isSolvable(self, target):
+        # check solvable:
+        # If N is odd, then puzzle instance is solvable if number of inversions is even in the input state.
+        # If N is even, puzzle instance is solvable if 
+        #   the blank is on an even row counting from the bottom (second-last, fourth-last, etc.) and number of inversions is odd.
+        #   the blank is on an odd row counting from the bottom (last, third-last, fifth-last, etc.) and number of inversions is even.
+        # For all other cases, the puzzle instance is not solvable.
         isOdd = False
         for i in range(6):
-            if targetH[i] == "0": continue
+            if target[i] == "0": continue
             for j in range(i, 6):
-                if targetH[j] == "0": continue
-
-                if ord(targetH[i]) > ord(targetH[j]):
+                if target[j] == "0": continue
+                if ord(target[i]) > ord(target[j]):
                     isOdd = not isOdd
-        if isOdd:
+        return not isOdd
+    
+    def bfs(self, target):
+        # use string to represent a board
+        target = "".join(str(x) for row in target for x in row)
+        if COMPLETED == target:
+            return 0
+        if not self.isSolvable(target):
             return -1
 
-        q = deque([(initial, 5)])
+        q = deque([(COMPLETED, 5)])
         visited = {}
-        canMoveTo = [
-            [1, 3], [0, 2, 4], [1, 5],
-            [0, 4], [1, 3, 5], [2, 4],
-        ]
-
         count = 0
         while q:
             qSize = len(q)
@@ -51,7 +57,7 @@ class Solution:
                     newH = "".join(arr)
                     # print(newH)
 
-                    if newH == targetH:
+                    if newH == target:
                         return count + 1
 
                     if newH not in visited:
@@ -59,3 +65,59 @@ class Solution:
                         visited[current] = True
             count += 1
         return -1
+
+    def aStarSearch(self, start):
+        # use string to represent a board
+        start = "".join(str(x) for row in start for x in row)
+        current = start
+        if start == COMPLETED:
+            return 0
+        if not self.isSolvable(start):
+            return -1
+        
+        def heuristic(board):
+            # f = g + h
+            # where g = cost from the start to current node
+            #       h = "heuristics", a smart guess from current node to the goal
+            # if 0 <= h <= h* (the actual cost)
+            # then we are guanrantee to achieve the shortest path
+            # if h = 0, it is dijkastra (and slow)
+            # if h = h*, it is fast
+            # This function only calculates h, using taxicab (Manhatann) distance to the COMPLETED state
+            total = 0
+            for i, x in enumerate(board):
+                x = ord(x) - ord("1")
+                row, col = i // 3, i % 3
+                if x == "0":
+                    total += abs(row - 1) + abs(col - 2)
+                else:
+                    total += abs(row -  x // 3) + abs(col - x // 3)
+            return total
+
+
+        # q = [(f=g+h, current, 5, g)]
+        q = [(0 + heuristic(current), current, 5, 0)]
+
+        visited = {}
+        while q:
+            f, current, pos, g = heappop(q)
+
+            for cell in canMoveTo[pos]:
+                # print(current, pos, cell)
+                arr = list(current)
+                arr[pos], arr[cell] = arr[cell], arr[pos]
+                newH = "".join(arr)
+                # print(newH)
+
+                if newH == COMPLETED:
+                    return g + 1
+
+                if newH not in visited:
+                    q.append(((g+1) + heuristic(newH), newH, cell, g+1))
+                    visited[current] = True
+        return -1
+
+
+
+    def slidingPuzzle(self, target: List[List[int]]) -> int:
+        return self.aStarSearch(target)
