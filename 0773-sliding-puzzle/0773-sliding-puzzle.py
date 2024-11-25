13c1fa854d71a54331dfd00f3289c5cf7dc49476
davidpangsw <davidpangsw@hotmail.com>
2024-11-24 19:37:21 -0800

    Time: 10 ms (50.79%), Space: 16.9 MB (27.27%) - LeetHub

--- 0773-sliding-puzzle/0773-sliding-puzzle.py
+++ 0773-sliding-puzzle/0773-sliding-puzzle.py
@@ -15,16 +15,12 @@ class Solution:
     def slidingPuzzle(self, target: List[List[int]]) -> int:
         # use string to represent a board
         initial = "123450"
-        # targetH = ""
-        # for row in target:
-        #     for x in row:
-        #         targetH += str(x)
         targetH = "".join(str(x) for row in target for x in row)
         if initial == targetH:
             return 0
 
         q = deque([(initial, 5, 0)])
-        visited = {}
+        visited = set()
         canMoveTo = [
             [1, 3], [0, 2, 4], [1, 5],
             [0, 4], [1, 3, 5], [2, 4],
@@ -49,5 +45,5 @@ class Solution:
 
                 if newH not in visited:
                     q.append((newH, cell, count + 1))
-                    visited[current] = True
+                    visited.add(current)
         return -1
