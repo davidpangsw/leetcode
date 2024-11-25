9bec8c693643939280fc808975b75526e2529410
davidpangsw <davidpangsw@hotmail.com>
2024-11-24 20:08:20 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0773-sliding-puzzle/0773-sliding-puzzle.py
+++ 0773-sliding-puzzle/0773-sliding-puzzle.py
@@ -13,13 +13,24 @@
 
 class Solution:
     def slidingPuzzle(self, target: List[List[int]]) -> int:
+        m, n = 2, 3
+
         # use string to represent a board
         initial = "123450"
         targetH = "".join(str(x) for row in target for x in row)
         if initial == targetH:
             return 0
 
-        q = deque([(initial, 5)])
+        # priority queue with this distance function
+        def calcDist(s):
+            result = 0
+            for i in range(m * n):
+                if s[i] != '0':
+                    num = ord(s[i]) - ord('1')
+                    result += abs(i // n - num // n) + abs(i % n - num % n)
+            return result
+
+        q = [(0, initial, 5, 0)]
         visited = {}
         canMoveTo = [
             [1, 3], [0, 2, 4], [1, 5],
@@ -28,26 +39,23 @@ class Solution:
 
         count = 0
         while q:
-            qSize = len(q)
-            for _ in range(qSize):
-                current, pos = q.popleft()
+            dist, current, pos, count = heapq.heappop(q)
 
-                for cell in canMoveTo[pos]:
-                    # print(current, pos, cell)
-                    arr = list(current)
-                    arr[pos], arr[cell] = arr[cell], arr[pos]
-                    newH = "".join(arr)
-                    # if pos <= cell:
-                    #     newH = current[:pos] + current[cell] + current[pos+1:cell] + current[pos] + current[cell+1:]
-                    # else:
-                    #     newH = current[:cell] + current[pos] + current[cell+1:pos] + current[cell] + current[pos+1:]
-                    # print(newH)
+            for cell in canMoveTo[pos]:
+                # print(current, pos, cell)
+                arr = list(current)
+                arr[pos], arr[cell] = arr[cell], arr[pos]
+                newH = "".join(arr)
+                # if pos <= cell:
+                #     newH = current[:pos] + current[cell] + current[pos+1:cell] + current[pos] + current[cell+1:]
+                # else:
+                #     newH = current[:cell] + current[pos] + current[cell+1:pos] + current[cell] + current[pos+1:]
+                # print(newH)
 
-                    if newH == targetH:
-                        return count + 1
+                if newH == targetH:
+                    return count + 1
 
-                    if newH not in visited:
-                        q.append((newH, cell))
-                        visited[current] = True
-            count += 1
+                if newH not in visited:
+                    heapq.heappush(q, (calcDist(newH), newH, cell, count + 1))
+                    visited[current] = True
         return -1
