17206add629ce9aef887560c7ca13e92031af817
davidpangsw <davidpangsw@hotmail.com>
2024-11-25 16:44:17 -0800

    Time: 15 ms (34.44%), Space: 17.7 MB (80.31%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.py
+++ 2924-find-champion-ii/2924-find-champion-ii.py
@@ -2,9 +2,6 @@ class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
         result = set(range(n))
         for e in edges:
-            u, v = e
-            result.discard(v)
-        if len(result) == 1:
-            return result.pop()
-        else:
-            return -1
+            result.discard(e[1])
+
+        return result.pop() if len(result) == 1 else -1
