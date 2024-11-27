c5b4963759fc64de1066e7a48b6b8e5d683f9b4d
davidpangsw <davidpangsw@hotmail.com>
2024-11-27 10:45:59 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
@@ -36,7 +36,7 @@ class Solution:
 
             # BFS from 0
             # steps on or before u are not affected.
-            q = deque([0])
+            q = deque([v])
             # q = deque(qs[u])
             # if steps[u] + 1 <= steps[v]:
             #     q.append(v)
