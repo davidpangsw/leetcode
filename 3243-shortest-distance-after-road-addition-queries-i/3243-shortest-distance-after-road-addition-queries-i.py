9e90f7c544e8d3d9efb715575cfccf9c54e6a3ef
davidpangsw <davidpangsw@hotmail.com>
2024-11-27 10:56:45 -0800

    Time: 59 ms (99.25%), Space: 18 MB (8.31%) - LeetHub

--- 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
@@ -1,20 +1,18 @@
 class Solution:
     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        results = []
         edges = [set([i+1]) for i in range(n-1)]
         edges.append(set())
 
-        results = []
-
-        # remember steps to each node
+        # remember number of steps to each node
         steps = list(range(n))
         # print(steps)
-
         for u, v in queries:
             edges[u].add(v)
             if steps[u] + 1 < steps[v]:
                 steps[v] = steps[u] + 1
 
-                # BFS starting from v
+                # BFS from v
                 q = deque([v])
                 while q:
                     node = q.popleft()
@@ -22,7 +20,7 @@ class Solution:
                         if steps[node] + 1 <= steps[dest]:
                             steps[dest] = steps[node] + 1
                             q.append(dest)
-            results.append(steps[n-1])
+            results.append(steps[-1])
             # print(steps)
        
         return results
