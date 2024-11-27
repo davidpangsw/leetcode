c5efe563d9cc28c984385ce97706a20834920c8a
davidpangsw <davidpangsw@hotmail.com>
2024-11-27 10:50:58 -0800

    Time: 55 ms (99.62%), Space: 17.7 MB (8.31%) - LeetHub

--- 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
@@ -1,38 +1,21 @@
 class Solution:
-    def shortestDistanceAfterQueriesDP(self, n: int, queries: List[List[int]]) -> List[int]:
-        edges = [set([i+1]) for i in range(n-1)]
-        dist = [n-1-i for i in range(n)]
-
-        results = []
-        for query in queries:
-            x, y = query
-            edges[x].add(y)
-            dist[x] = min(dist[x], 1 + dist[y])
-            for node in range(x - 1, -1, -1):
-                for dest in edges[node]:
-                    if dest > x:
-                        continue
-                    dist[node] = min(dist[node], 1 + dist[dest])
-            results.append(dist[0])
-        return results
-
-
-
     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
         edges = [set([i+1]) for i in range(n-1)]
         edges.append(set())
 
         results = []
+
+        # remember steps to each node
         steps = list(range(n))
         # print(steps)
 
         for u, v in queries:
             edges[u].add(v)
-
             if steps[u] + 1 < steps[v]:
                 steps[v] = steps[u] + 1
-                q = deque([v])
 
+                # BFS starting from v
+                q = deque([v])
                 while q:
                     node = q.popleft()
                     for dest in edges[node]:
