0c83233f2ad4744a3d5b2027f6ea174ef7ba9e90
davidpangsw <davidpangsw@hotmail.com>
2024-11-27 10:02:11 -0800

    Time: 467 ms (68.8%), Space: 17.6 MB (8.31%) - LeetHub

--- 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.py
@@ -1,23 +1,14 @@
 class Solution:
     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
-        # dist = [[inf for j in range(n)] for i in range(n)]
-        # for i in range(n):
-        #     for j in range(i, n):
-        #         dist[i][j] = j-i
-        # [print(row) for row in dist]
-        # print()
         edges = [set([i+1]) for i in range(n-1)]
 
         results = []
         for query in queries:
-            # print(query)
-            x, y = query
-            edges[x].add(y)
+            edges[query[0]].add(query[1])
 
             # BFS from 0
             q = deque([0])
-            visited = [False for i in range(n)]
-            result = inf
+            visited = [False] * n
             step = 0
             while q:
                 qSize = len(q)
@@ -29,20 +20,12 @@ class Solution:
 
                     # print(cost, node)
                     if node == n-1:
-                        result = step
+                        results.append(step)
                         q = None
                         break
-                    else:
-                        for destNode in edges[node]:
-                            q.append(destNode)
-                step += 1
-            results.append(result)
 
-            # # update all a->x->y->b
-            # This method is for dense graph
-            # for a in range(0, x+1):
-            #     for b in range(y, n):
-            #         dist[a][b] = min(dist[a][b], dist[a][x] + 1 + dist[y][b])
-            # result.append(dist[0][n-1])
+                    for destNode in edges[node]:
+                        q.append(destNode)
+                step += 1
        
         return results
