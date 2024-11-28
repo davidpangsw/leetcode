d6a31a9885d8e23ebc691b2d0f9f622c9890289a
davidpangsw <davidpangsw@hotmail.com>
2024-11-27 22:04:41 -0800

    Time: 904 ms (90.12%), Space: 44.6 MB (49.59%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
@@ -1,4 +1,3 @@
-DIRS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 class Solution:
     def minimumObstacles(self, grid: List[List[int]]) -> int:
         # BFS + priority queue
@@ -15,8 +14,6 @@ class Solution:
             if x == m-1 and y == n-1:
                 return cost
 
-
-            # print(x, y)
             if x > 0:
                 X, Y = x - 1, y
                 if mem[X][Y] > cost + grid[X][Y]:
@@ -38,10 +35,5 @@ class Solution:
                     mem[X][Y] = cost + grid[X][Y]
                     heappush(q, (cost + grid[X][Y], X, Y))
 
-            # for dx, dy in DIRS:
-            #     X, Y = x + dx, y + dy
-            #     if 0 <= X < m and 0 <= Y < n and mem[X][Y] > cost + grid[X][Y]:
-            #         mem[X][Y] = cost + grid[X][Y]
-            #         heappush(q, (cost + grid[X][Y], X, Y))
         raise "Path not found"
 
