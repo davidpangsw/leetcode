333c9d782d27afad010a73717f7a936f0449067c
davidpangsw <davidpangsw@hotmail.com>
2024-11-27 22:06:23 -0800

    Time: 763 ms (94.24%), Space: 44.5 MB (49.59%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
@@ -3,37 +3,51 @@ class Solution:
         # BFS + priority queue
         m, n = len(grid), len(grid[0])
         mem = [[inf for j in range(n)] for i in range(m)]
+        mem[0][0] = 0
 
         # cost, x, y
-        q = [(0, 0, 0)]
+        # q = [(0, 0, 0)]
+        q = deque([(0, 0, 0)])
+
+        def update(cost, X, Y):
+            if mem[X][Y] > cost + grid[X][Y]:
+                mem[X][Y] = cost + grid[X][Y]
+                if grid[X][Y]:
+                    q.append((cost + grid[X][Y], X, Y))
+                else:
+                    q.appendleft((cost + grid[X][Y], X, Y))
 
         while q:
-            cost, x, y = heappop(q)
+            cost, x, y = q.popleft()
             # print(cost, x, y)
 
             if x == m-1 and y == n-1:
                 return cost
 
             if x > 0:
-                X, Y = x - 1, y
-                if mem[X][Y] > cost + grid[X][Y]:
-                    mem[X][Y] = cost + grid[X][Y]
-                    heappush(q, (cost + grid[X][Y], X, Y))
+                update(cost, x-1, y)
+                # X, Y = x - 1, y
+                # if mem[X][Y] > cost + grid[X][Y]:
+                #     mem[X][Y] = cost + grid[X][Y]
+                #     heappush(q, (cost + grid[X][Y], X, Y))
             if x < m-1:
-                X, Y = x + 1, y
-                if mem[X][Y] > cost + grid[X][Y]:
-                    mem[X][Y] = cost + grid[X][Y]
-                    heappush(q, (cost + grid[X][Y], X, Y))
+                update(cost, x+1, y)
+                # X, Y = x + 1, y
+                # if mem[X][Y] > cost + grid[X][Y]:
+                #     mem[X][Y] = cost + grid[X][Y]
+                #     heappush(q, (cost + grid[X][Y], X, Y))
             if y > 0:
-                X, Y = x, y - 1
-                if mem[X][Y] > cost + grid[X][Y]:
-                    mem[X][Y] = cost + grid[X][Y]
-                    heappush(q, (cost + grid[X][Y], X, Y))
+                update(cost, x, y - 1)
+                # X, Y = x, y - 1
+                # if mem[X][Y] > cost + grid[X][Y]:
+                #     mem[X][Y] = cost + grid[X][Y]
+                #     heappush(q, (cost + grid[X][Y], X, Y))
             if y < n-1:
-                X, Y = x, y + 1
-                if mem[X][Y] > cost + grid[X][Y]:
-                    mem[X][Y] = cost + grid[X][Y]
-                    heappush(q, (cost + grid[X][Y], X, Y))
+                update(cost, x, y + 1)
+                # X, Y = x, y + 1
+                # if mem[X][Y] > cost + grid[X][Y]:
+                #     mem[X][Y] = cost + grid[X][Y]
+                #     heappush(q, (cost + grid[X][Y], X, Y))
 
         raise "Path not found"
 
