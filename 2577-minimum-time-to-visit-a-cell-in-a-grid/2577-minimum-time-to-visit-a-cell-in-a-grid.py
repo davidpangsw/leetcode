2cac7479bc38a40745085e9f84c5dccf25c85434
davidpangsw <davidpangsw@hotmail.com>
2024-11-28 21:46:37 -0800

    Time: 522 ms (97.35%), Space: 30.6 MB (82.85%) - LeetHub

--- 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.py
+++ 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.py
@@ -1,39 +1,25 @@
-DIRS = [(-1,0), (1,0), (0,-1), (0,1)]
 class Solution:
     def minimumTime(self, grid: List[List[int]]) -> int:
-        # ensure we can go back and forth first
-        if grid[1][0] > 1 and grid[0][1] > 1:
+        DIR = ((-1, 0), (0, 1), (1, 0), (0, -1))
+        if grid[0][1] > 1 and grid[1][0] > 1:
             return -1
-
-        m, n = len(grid), len(grid[0])
+        n, m = len(grid), len(grid[0])
         q = [(0, 0, 0)]
-        # q = [(0, 0, 0, 0)]
-        visited = [[False for _ in range(n)] for _ in range(m)]
+        visit = [[False] * m for _ in range(n)]
+        visit[0][0] = True
         while q:
-            # print(q)
-            cost, x, y = heappop(q)
-            # weight, cost, x, y = heappop(q)
-
-            for dx, dy in DIRS:
-                X, Y = x+dx, y+dy
-
-                if not (0 <= X < m and 0 <= Y < n):
+            t, x, y = heappop(q)
+            for dx, dy in DIR:
+                nx, ny = x + dx, y + dy
+                if nx < 0 or nx >= n or ny < 0 or ny >= m or visit[nx][ny]:
                     continue
 
-                if visited[X][Y]:
-                    continue
-            
-                nextCost = cost + 1
-                if nextCost < grid[X][Y]:
-                    # go back and forth until time enough
-                    # which is equivalent to "wait" even number of seconds
-                    nextCost = grid[X][Y] + ((grid[X][Y] & 1) ^ (nextCost & 1))
+                nt = t + 1
+                if grid[nx][ny] > nt:
+                    nt += (grid[nx][ny] - t) // 2 * 2
 
-                if X == m-1 and Y == n-1:
-                    return nextCost
-
-                visited[X][Y] = True
-                heappush(q, (nextCost, X, Y))
-                # heappush(q, (nextCost + (m-1-X+n-1-Y), nextCost, X, Y))
-
+                if nx == n - 1 and ny == m - 1:
+                    return nt
+                visit[nx][ny] = True
+                heappush(q, (nt, nx, ny))
         return -1
