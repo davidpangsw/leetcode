5d68507cc2ce47cc0b1caf696efcd0ee2d887e4a
davidpangsw <davidpangsw@hotmail.com>
2024-11-28 21:28:41 -0800

    Time: 631 ms (89.38%), Space: 30.7 MB (78.47%) - LeetHub

--- 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.py
+++ 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.py
@@ -2,12 +2,12 @@ class Solution:
     def minimumTime(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
 
-
         # ensure we can go back and forth first
         if grid[1][0] > 1 and grid[0][1] > 1:
             return -1
 
         q = [(0, 0, 0)]
+        # q = [(0, 0, 0, 0)]
         visited = [[False for _ in range(n)] for _ in range(m)]
         while q:
             # print(q)
@@ -28,7 +28,7 @@ class Solution:
                     # go back and forth until time enough
                     # which is equivalent to "wait" even number of seconds
                     # and then go 1 step (that is, odd)
-                    nextCost = grid[X][Y] + (1 - (grid[X][Y] - cost) % 2)
+                    nextCost = grid[X][Y] + (1 - (grid[X][Y] - cost) & 1)
                 else:
                     nextCost = cost + 1
 
