caa8bb6a39c6c0a7edd1f29201c2a583d21f781b
davidpangsw <davidpangsw@hotmail.com>
2024-11-28 20:55:34 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.py
+++ 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.py
@@ -8,33 +8,32 @@ class Solution:
         while q:
             # print(q)
             cost, x, y = heappop(q)
-            # print(cost, x, y)
             if x == m-1 and y == n-1:
-                return cost
-            # print(visited[x][y])
+                return cost -  (m-1-x+n-1-y)
+
             if visited[x][y]:
                 continue
             visited[x][y] = True
 
             for dx, dy in [[-1,0], [1,0], [0,-1], [0,1]]:
                 X, Y = x+dx, y+dy
-                # print(X, Y)
 
                 if not (0 <= X < m and 0 <= Y < n):
                     continue
 
             
                 if cost + 1 < grid[X][Y]:
-                    if cost == 0: # no grid to go backward, view it later
+                    if cost == 0: # no grid to go backward, view (0,0) again later
                         visited[0][0] = False
                         continue
                     # go back and forth until time enough
                     # which is equivalent to "wait" even number of seconds
                     # and then go 1 step (that is, odd)
                     if (grid[X][Y] - cost) % 2 == 0:
-                        heappush(q, [grid[X][Y] + 1, X, Y])
+                        nextCost = grid[X][Y] + 1
                     else:
-                        heappush(q, [grid[X][Y], X, Y])
+                        nextCost = grid[X][Y]
                 else:
-                    heappush(q, [cost + 1, X, Y])
+                    nextCost = cost + 1
+                heappush(q, [nextCost + (m-1-X+n-1-Y), X, Y])
         return -1
