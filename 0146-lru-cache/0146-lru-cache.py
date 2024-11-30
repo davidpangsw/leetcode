6fedba14930d239dff10ef91a300e1bdbbfc76c2
davidpangsw <davidpangsw@hotmail.com>
2024-11-29 17:25:58 -0800

    Time: 114 ms (84.47%), Space: 78.9 MB (5.2%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -4,21 +4,18 @@ class LRUCache:
         self.data = OrderedDict()
     
     def get(self, key: int) -> int:
-        # print("get", self.data, len(self.data), self.capacity)
         if key not in self.data:
             return -1
         self.data.move_to_end(key)
         return self.data[key]
 
     def put(self, key: int, value: int) -> None:      
-        # print("put", self.data, len(self.data), self.capacity)
-
         self.data[key] = value
         self.data.move_to_end(key)
 
         # delete LRU item
         if len(self.data) > self.capacity:
-            _ = self.data.popitem(False)
+            self.data.popitem(False)
             
 
 
