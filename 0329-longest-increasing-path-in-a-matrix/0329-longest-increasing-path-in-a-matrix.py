01d97b03ec881bbdccbb45ed4108a73d2cc9ea73
davidpangsw <davidpangsw@hotmail.com>
2024-11-30 23:21:25 -0800

    Time: 143 ms (70.56%), Space: 19.3 MB (57.29%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -7,35 +7,37 @@ class Solution:
         adjLists = [[[] for _ in range(n)] for _ in range(m)]
 
         # DFS
-        q = []
 
-        # find all starting points
-        for i, row in enumerate(matrix):
-            for j, cell in enumerate(row):
-                inDegree = 0
-                for di, dj in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-                    I, J = i+di, j+dj
-                    if 0 <= I < m and 0 <= J < n:
-                        if matrix[I][J] < matrix[i][j]:
-                            adjLists[I][J].append((i, j))
-                            inDegree += 1
-                if inDegree == 0:
-                    q.append((i, j))
-
+        # # find all starting points
+        # for i, row in enumerate(matrix):
+        #     for j, cell in enumerate(row):
+        #         inDegree = 0
+        #         for di, dj in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+        #             I, J = i+di, j+dj
+        #             if 0 <= I < m and 0 <= J < n:
+        #                 if matrix[I][J] < matrix[i][j]:
+        #                     adjLists[I][J].append((i, j))
+        #                     inDegree += 1
+        #         if inDegree == 0:
+        #             q.append((i, j))
+               
         def dfs(i, j):
             if results[i][j]:
                 return results[i][j]
-            
             l = 1
-            for I, J in adjLists[i][j]:
-                l = max(l, 1 + dfs(I, J))
-            
+            # for I, J in adjLists[i][j]:
+            for di, dj in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+                I, J = i+di, j+dj
+                if 0 <= I < m and 0 <= J < n:
+                    if matrix[I][J] > matrix[i][j]:
+                        l = max(l, 1 + dfs(I, J))
             results[i][j] = l
             return l
-        
+
         result = 0
-        for x, y in q:
-            result = max(result, dfs(x, y))
+        for x in range(m):
+            for y in range(n):
+                result = max(result, dfs(x, y))
         return result
         
 
