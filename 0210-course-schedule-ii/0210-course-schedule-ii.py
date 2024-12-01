4a3e85b4305389ca9b02653bf5f8305bde036511
davidpangsw <davidpangsw@hotmail.com>
2024-11-30 17:55:53 -0800

    Time: 2 ms (83.21%), Space: 18.6 MB (33.84%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,17 +1,15 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        # adjLists = [[] for _ in range(numCourses)]
-        # inDegrees = [0] * numCourses
-        adjLists = defaultdict(list)
-        inDegrees = defaultdict(int)
+        adjLists = [[] for _ in range(numCourses)]
+        inDegrees = [0] * numCourses
         for v, u in prerequisites:
             adjLists[u].append(v)
             inDegrees[v] += 1
         
         # Topological sort, using Kahn's Algorithm
         q = deque()
-        for i in range(numCourses):
-            if inDegrees[i] == 0:
+        for i, x in enumerate(inDegrees):
+            if x == 0:
                 q.append(i)
         
         result = []
