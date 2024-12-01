4ca7d813f00778fe8a81818a2f2862d37c78eb9a
davidpangsw <davidpangsw@hotmail.com>
2024-11-30 18:22:53 -0800

    Time: 43 ms (87.32%), Space: 25.4 MB (75.68%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -2,7 +2,7 @@ class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
         if n == 1:
             return [0]
-            
+
         adjLists = [[] for _ in range(n)]
         degrees = [0] * n
 
@@ -14,21 +14,24 @@ class Solution:
         
         # BFS by levels
         # Kahn's Algorithm (Topo-sort)
-        q = deque()
+        q = []
         for i, x in enumerate(degrees):
             if x == 1:
                 q.append(i)
 
+        print(q)
         while q:
-            print(q)
-            qSize = len(q)
-            if qSize <= 2:
-                return list(q)
-
-            for _ in range(qSize):
-                cur = q.popleft()
+            newQ = []
+            for cur in q:
                 for adj in adjLists[cur]:
                     # remove the edge
                     degrees[adj] -= 1
                     if degrees[adj] == 1:
-                        q.append(adj)
+                        newQ.append(adj)
+            if not newQ:
+                return q
+            elif len(newQ) == 1:
+                return newQ
+            else:
+                q = newQ
+            print(q)
