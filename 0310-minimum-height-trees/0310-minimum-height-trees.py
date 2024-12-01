21831176ed6ed8bfb33364f774e86209d41b3835
davidpangsw <davidpangsw@hotmail.com>
2024-11-30 18:27:37 -0800

    Time: 43 ms (87.32%), Space: 29.4 MB (15.75%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -3,20 +3,18 @@ class Solution:
         if n == 1:
             return [0]
 
-        adjLists = [[] for _ in range(n)]
-        degrees = [0] * n
+        adjLists = [set() for _ in range(n)]
 
         for u, v in edges:
-            degrees[u] += 1
-            degrees[v] += 1
-            adjLists[u].append(v)
-            adjLists[v].append(u)
+            adjLists[u].add(v)
+            adjLists[v].add(u)
         
         # BFS by levels
         # Kahn's Algorithm (Topo-sort)
         q = []
-        for i, x in enumerate(degrees):
-            if x == 1:
+        for i, x in enumerate(adjLists):
+            # if the degree is 1, it must be a leaf
+            if len(x) == 1:
                 q.append(i)
 
         # print(q)
@@ -25,10 +23,10 @@ class Solution:
             for cur in q:
                 for adj in adjLists[cur]:
                     # remove the edge (just the degree)
-                    degrees[adj] -= 1
-                    if degrees[adj] == 1:
+                    adjLists[adj].remove(cur)
+                    if len(adjLists[adj]) == 1:
                         newQ.append(adj)
-            if not newQ: # here is why we kept prev queue
+            if not newQ: # this is why we kept prev queue
                 return q
             elif len(newQ) == 1:
                 return newQ
