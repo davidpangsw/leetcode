5ed4121b40d8363b3543f97a27637156f3730c4c
davidpangsw <davidpangsw@hotmail.com>
2024-11-30 16:37:49 -0800

    Time: 3 ms (57.43%), Space: 17.5 MB (5.25%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        arr.sort()
+        for i, x in enumerate(arr):
+            d = (x << 1)
+
+            if x > 0:
+                ind = bisect_left(arr, d)
+            elif x < 0:
+                ind = bisect_left(arr, d, 0, i)
+            else:
+                if i > 0:
+                    ind = i - 1
+                else:
+                    ind = i
+            if ind == len(arr):
+                break
+            if arr[ind] == d:
+                return True
+        return False
