2bcf99038500351d8a6fe88a05bca4eaf5419491
davidpangsw <davidpangsw@hotmail.com>
2024-11-30 16:46:10 -0800

    Time: 3 ms (57.43%), Space: 17.3 MB (5.25%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -3,15 +3,14 @@ class Solution:
         zero = False
         data = set()
         for i, x in enumerate(arr):
-            data.add(x)
             if x == 0:
                 if zero:
                     return True
                 zero = True
+            else:
+                data.add(x)
 
         for i, x in enumerate(arr):
-            if x == 0:
-                continue
             if (x << 1) in data:
                 return True
         return False
