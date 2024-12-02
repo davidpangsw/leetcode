55330c309d9a981b9996d130042258016c9bf975
davidpangsw <davidpangsw@hotmail.com>
2024-12-02 01:57:07 -0800

    Time: 59 ms (75.76%), Space: 26.4 MB (34.52%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices)
-        maxAfter = [0] * n
-        for i in range(n-2, -1, -1):
-            maxAfter[i] = max(maxAfter[i+1], prices[i+1])
-        
-        maxProfit = 0
-        for i, x in enumerate(prices):
-            profit =  maxAfter[i] - prices[i] 
-            if profit > maxProfit:
-                maxProfit = profit
-        return maxProfit
-
+        result = 0
+        curMin = inf
+        for x in prices:
+            if x > curMin:
+                result = max(result, x - curMin)
+            else:
+                curMin = x
+        return result
