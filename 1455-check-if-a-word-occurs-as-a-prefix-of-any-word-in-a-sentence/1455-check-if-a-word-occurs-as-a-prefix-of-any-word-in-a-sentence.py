b11b5944892b6929c02889b2baaf03236b1c72db
davidpangsw <davidpangsw@hotmail.com>
2024-12-01 17:05:14 -0800

    Time: 0 ms (100%), Space: 17.3 MB (5.67%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
@@ -5,25 +5,39 @@ class Solution:
         #         return i + 1
         # return -1
 
-        i = j = 0
+        count = 0
         word = 1
-        while i < len(sentence):
-            # print(sentence[i], j, result)
-            
-            if sentence[i] == searchWord[j]:
-                i += 1
-                j += 1
-                if j == len(searchWord):
+        for x in sentence:
+            if count != -1 and x == searchWord[count]:
+                count += 1
+                if count == len(searchWord):
                     return word
-                continue
-            
-            # go to next word
-            i += 1
-            while i < len(sentence) and sentence[i-1] != " ":
-                i += 1
-            word += 1
-
-            # reset the count
-            j = 0
-
+            elif x == ' ':
+                count = 0
+                word += 1
+            else:
+                count = -1
         return -1
+
+        # i = j = 0
+        # word = 1
+        # while i < len(sentence):
+        #     # print(sentence[i], j, result)
+            
+        #     if sentence[i] == searchWord[j]:
+        #         i += 1
+        #         j += 1
+        #         if j == len(searchWord):
+        #             return word
+        #         continue
+            
+        #     # go to next word
+        #     i += 1
+        #     while i < len(sentence) and sentence[i-1] != " ":
+        #         i += 1
+        #     word += 1
+
+        #     # reset the count
+        #     j = 0
+
+        # return -1
