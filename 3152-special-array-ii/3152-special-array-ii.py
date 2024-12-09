01fdab582ce1607cc3670eb2ebdbd5dcf578ebb4
davidpangsw <davidpangsw@hotmail.com>
2024-12-09 04:04:24 -0800

    Time: 31 ms (93.98%), Space: 46.6 MB (53.04%) - LeetHub

--- 3152-special-array-ii/3152-special-array-ii.py
+++ 3152-special-array-ii/3152-special-array-ii.py
@@ -4,7 +4,7 @@ class Solution:
 
         count = 0
         for i in range(1, len(nums)):
-            count += not ((nums[i] ^ nums[i-1]) & 1)
+            count += not ((nums[i] & 1) ^ (nums[i-1] & 1))
             prefixCounts[i] = count
         # print(prefixCounts)
         
