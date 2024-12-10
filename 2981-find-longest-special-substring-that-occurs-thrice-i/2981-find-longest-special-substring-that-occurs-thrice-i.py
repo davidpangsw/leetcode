2fc2fb25a2a56bcabcecf0e7a359c8ecc2ecfa62
davidpangsw <davidpangsw@hotmail.com>
2024-12-09 23:27:15 -0800

    Time: 14 ms (65.41%), Space: 17.1 MB (17.44%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
@@ -17,10 +17,16 @@ class Solution:
         result = -1
         for i in range(n):
             x = ord(s[i]) - ord('a')
+
+            # update max counts Thrice (only when maxCountsTwice[x] exists)
             if maxCountsTwice[x]:
                 countsThrice = min(maxCountsTwice[x], counts[i])
                 result = max(result, countsThrice)
+            
+            # update max counts Twice
             countsTwice = min(maxCounts[x], counts[i])
             maxCountsTwice[x] = max(maxCountsTwice[x], countsTwice)
+            
+            # update max counts
             maxCounts[x] = max(maxCounts[x], counts[i])
         return result 
