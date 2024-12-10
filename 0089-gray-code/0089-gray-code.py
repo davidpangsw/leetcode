e819117a8d1958109c2091e5a6d4208f3c36fb96
davidpangsw <davidpangsw@hotmail.com>
2024-12-09 19:09:08 -0800

    Time: 11 ms (32.49%), Space: 20.5 MB (44.92%) - LeetHub

--- 0089-gray-code/0089-gray-code.py
+++ 0089-gray-code/0089-gray-code.py
@@ -1,24 +1,25 @@
 class Solution:
     def grayCode(self, n: int) -> List[int]:
+        # 0, 1
+        # 00, 10, 11, 01
+        # 000, 100, 110, 010, 011, 111, 101, 001
+
         # 0  ,                               1
         # 00 ,           01 , 11 ,           10
         # 000, 001, 011, 010, 110, 111, 101, 100
         # ...
-        def helper(n: int):
+        def helper(n: int, result):
             if n == 1:
-                yield 0
-                yield 1
+                result.append(0)
+                result.append(1)
                 return
 
-            gen = helper(n-1)
-            while True:
-                try:
-                    x = next(gen)
-                    y = next(gen)
-                    yield (x << 1)
-                    yield (x << 1) + 1
-                    yield (y << 1) + 1
-                    yield (y << 1)
-                except StopIteration:
-                    break
-        return list(helper(n))
+            helper(n-1, result)
+            m = len(result)
+            for i in range(m):
+                result[i] = result[i] << 1
+            for i in range(m-1, -1, -1):
+                result.append(result[i] + 1)
+        result = []
+        helper(n, result)
+        return result
