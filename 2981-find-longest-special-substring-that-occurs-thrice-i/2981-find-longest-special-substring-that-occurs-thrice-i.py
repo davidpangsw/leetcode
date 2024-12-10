f616eb7465dfd31f11c9f4cdf1f8f017d8f0c430
davidpangsw <davidpangsw@hotmail.com>
2024-12-09 23:49:50 -0800

    Time: 3 ms (99.73%), Space: 17.2 MB (17.44%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
@@ -6,7 +6,6 @@ class Solution:
 
         count = 1
         for i in range(1, n):
-            x = ord(s[i]) - ord('a')
             if s[i] == s[i-1]:
                 count += 1
             else:
