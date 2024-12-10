3c13eb56bcfa1ee01fb679f46d8a70809149ee1c
davidpangsw <davidpangsw@hotmail.com>
2024-12-09 23:54:02 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2982-find-longest-special-substring-that-occurs-thrice-ii/2982-find-longest-special-substring-that-occurs-thrice-ii.py
+++ 2982-find-longest-special-substring-that-occurs-thrice-ii/2982-find-longest-special-substring-that-occurs-thrice-ii.py
@@ -17,7 +17,6 @@ class Solution:
         for x in range(26):
             if not table[x]:
                 continue
-            s = sorted(table[x])
             if len(s) == 1:
                 # 1 number
                 # a-2
@@ -28,6 +27,7 @@ class Solution:
                 # min(a, b-1)
                 # a-1
                 # X a-2 X
+                s = sorted(table[x])
                 result = max(result, s[1]-2, min(s[0], s[1]-1), s[0]-1)
             else:
                 # 3 numbers (a <= b <= c)
@@ -35,6 +35,7 @@ class Solution:
                 # min(b, c-1)  ; X min(a, c-1) X
                 # b-1          ; X a-1 X
                 # a
+                s = sorted(table[x])
                 result = max(result, s[-1] - 2, min(s[-2], s[-1]-1), s[-2] - 1, s[-3])
 
         return result if result > 0 else -1
