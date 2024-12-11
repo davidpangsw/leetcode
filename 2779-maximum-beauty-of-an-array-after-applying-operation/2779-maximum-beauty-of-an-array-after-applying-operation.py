373c7d3e27f14615d579eaa6c1297706b0eef350
davidpangsw <davidpangsw@hotmail.com>
2024-12-10 20:46:28 -0800

    Time: 339 ms (28.2%), Space: 28.1 MB (39.52%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
@@ -8,9 +8,9 @@ class Solution:
         p = 0 # nums[p, i] is like a queue of intervals
         for i in range(1, len(nums)):
             # dequeue all non-overlapping intervals
-            # we don't need to dequeue all in practice, as it doesn't affect the maximum count
-            while nums[p] < nums[i] - 2 * k:
-                p += 1
+            # binary search the left index
+            p = bisect_left(nums, nums[i] - 2 * k, lo=p, hi=i)
+
             # now, all intervals from p to i are overlapping
             result = max(result, i - p + 1)
         return result
