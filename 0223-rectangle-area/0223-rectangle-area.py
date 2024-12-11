44c28605d8a3a3053395f767d6c8f4d8f851bc72
davidpangsw <davidpangsw@hotmail.com>
2024-12-11 13:10:16 -0800

    Time: 6 ms (16.26%), Space: 17.3 MB (18.52%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.py
+++ 0223-rectangle-area/0223-rectangle-area.py
@@ -7,13 +7,12 @@ class Solution:
 
         total = (ax2-ax1) * (ay2-ay1) + (bx2-bx1) * (by2-by1)
 
-        # find the overlap corner
+        # find the overlap corners
         cx1, cy1 = max(ax1, bx1), max(ay1, by1)
         cx2, cy2 = min(ax2, bx2), min(ay2, by2)
-
         # print(cx1, cy1, cx2, cy2)
 
-        # if the overlap is possible
+        # if the overlap is possible, subtract the area back
         if (bx1 <= cx1 < bx2 and by1 <= cy1 < by2) and (bx1 < cx2 <= bx2 and by1 < cy2 <= by2):
             total -= (cx2-cx1) * (cy2-cy1) 
         return total
