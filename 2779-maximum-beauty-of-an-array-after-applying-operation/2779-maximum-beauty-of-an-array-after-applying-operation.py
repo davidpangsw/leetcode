8f247f2dc8da8e55f14c34e4576de8c63b692ff7
davidpangsw <davidpangsw@hotmail.com>
2024-12-10 20:52:06 -0800

    Time: 399 ms (24.78%), Space: 25.2 MB (100%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
@@ -3,15 +3,23 @@ class Solution:
         # consider the intervals [nums[i] - k, nums[i] + k]
         # the answer would be the maximum count of overlaps
         # use a queue to store all overlapping intervals (represented by two pointer)
-        nums.sort()
+        q = deque()
+        heapify(nums)
+        q.append(heappop(nums))
         result = 1
-        p = 0 # nums[p, i] is like a queue of intervals
-        for i in range(1, len(nums)):
-            # dequeue all non-overlapping intervals
-            # binary search the left index
-            p = bisect_left(nums, nums[i] - 2 * k, lo=p, hi=i)
+        while nums:
+            x = heappop(nums)
+            q.append(x)
 
-            # now, all intervals from p to i are overlapping
-            result = max(result, i - p + 1)
+            # dequeue all non-overlapping intervals
+            # we don't need to dequeue all in practice, as it doesn't affect the maximum count
+            while q[0] + k < x - k:
+                q.popleft()
+
+            # # binary search the left index
+            # p = bisect_left(nums, nums[i] - 2 * k, lo=p, hi=i)
+
+            # now, all intervals in queue are overlapping
+            result = max(result, len(q))
         return result
 
