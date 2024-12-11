9adf5115922d31f04e6dad14bd0d7e0ab8a37066
davidpangsw <davidpangsw@hotmail.com>
2024-12-10 20:29:39 -0800

    Time: 239 ms (44.01%), Space: 28.2 MB (31.74%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
@@ -1,19 +1,16 @@
 class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
         # consider the intervals [nums[i] - k, nums[i] + k]
-        # make all interval exclusive
-        # combine overlaps and store the counts
-        # the answer would be the maximum count
+        # the answer would be the maximum count of overlaps
+        # use a queue to store all overlapping intervals (represented by two pointer)
         nums.sort()
         result = 1
-        
-        # intervals = deque()
-        p = 0 # like a deque of intervals
+        p = 0 # nums[p, i] is like a queue of intervals
         for i in range(1, len(nums)):
             # dequeue all non-overlapping intervals
-            if nums[p] + k < nums[i] - k:
+            while nums[p] + k < nums[i] - k:
                 p += 1
-            # now, all intervals from i to p are overlapping
+            # now, all intervals from p to i are overlapping
             result = max(result, i - p + 1)
         return result
 
