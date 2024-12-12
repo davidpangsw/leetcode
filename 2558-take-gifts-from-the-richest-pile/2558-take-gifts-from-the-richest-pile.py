2bb68cb7a8ae7feb4b358c9eb74af2e010152f52
davidpangsw <davidpangsw@hotmail.com>
2024-12-11 19:32:58 -0800

    Time: 5 ms (53.2%), Space: 17.6 MB (8.46%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -2,7 +2,7 @@ class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
         arr = [-gifts[i] for i in range(len(gifts))]
         heapify(arr)
-        for i in range(k):
+        for _ in range(k):
             p = heappop(arr)
             heappush(arr, -int(sqrt(-p)))
         return -sum(arr)
