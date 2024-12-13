a9ad63ccf3475a84fc9632369b5149ad2667998e
davidpangsw <davidpangsw@hotmail.com>
2024-12-13 10:58:22 -0800

    Time: 51 ms (98.18%), Space: 29.1 MB (98.71%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.py
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.py
@@ -1,17 +1,55 @@
 class Solution:
     def findScore(self, nums: List[int]) -> int:
+        # Observations:
+        #   1. If an element is smaller than its neighbourhoods, than it must be chosen
+        #   2. If an element is chosen, then its greater neighbourhoods must not be chosen
+        # Loop the elements, every time a local minimum is identified, add all the elements on and before it alternatively
+        # This can achieve O(n)
+        score = 0
+        n = len(nums)
+
+        left = 0
+        i = 1
+        while i < n:
+            if nums[i] < nums[i-1]:
+                i += 1
+                continue
+            
+            # print(nums[left: i])
+            
+            # now, nums[i-1] is a local minimum
+            for j in range(i-1, left-1, -2):
+                score += nums[j]
+            
+            left = i+1
+            i += 2
+        
+        # print(nums[left: n])
+        
+        if left < n:
+            # now, nums[i-1] is a local minimum
+            for j in range(n-1, left-1, -2):
+                score += nums[j]
+
+        return score
+
+
+
+
+    def findScoreSlow(self, nums: List[int]) -> int:
         score = 0
 
-        visited = set()
         arr = [(x, i) for i, x in enumerate(nums)]
         heapify(arr)
         while arr:
             x, i = heappop(arr)
-            if i in visited:
+            if not nums[i]:
                 continue
 
             score += x
-            visited.add(i-1)
-            visited.add(i+1)
+            if i > 0:
+                nums[i-1] = None
+            if i < len(nums) - 1:
+                nums[i+1] = None
 
         return score
