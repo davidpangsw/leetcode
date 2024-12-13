0c3e31d57fbc5063048fd7bf31c046ed2ab58a39
davidpangsw <davidpangsw@hotmail.com>
2024-12-13 00:28:05 -0800

    Time: 562 ms (47.64%), Space: 37.5 MB (81.03%) - LeetHub

---
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findScore(self, nums: List[int]) -> int:
+        score = 0
+
+        arr = [(x, i) for i, x in enumerate(nums)]
+        heapify(arr)
+        while arr:
+            x, i = heappop(arr)
+            if nums[i] is None:
+                continue
+            score += x
+            if i > 0:
+                nums[i-1] = None
+            if i < len(nums) - 1:
+                nums[i+1] = None
+
+        return score
