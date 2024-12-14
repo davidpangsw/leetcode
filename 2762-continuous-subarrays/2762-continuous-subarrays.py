72ea26a03e100e1da4fb7166b146a8122a306ecf
davidpangsw <davidpangsw@hotmail.com>
2024-12-14 07:52:33 -0800

    Time: 926 ms (18.07%), Space: 27.5 MB (40.97%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.py
+++ 2762-continuous-subarrays/2762-continuous-subarrays.py
@@ -1,32 +1,38 @@
 class Solution:
     def continuousSubarrays(self, nums: List[int]) -> int:
+        left  = right = 0
+
         total = 0
-        left = 0
-        curMax = curMin = nums[0]
-        for right, x in enumerate(nums):
-            # print(nums[left:right], curMax, curMin)
-            curMax = max(curMax, x)
-            curMin = min(curMin, x)
-            if curMax - curMin > 2:
-                # process the subarray
-                total += (right - left + 1) * (right - left) // 2
+        counts = defaultdict(int)
+        while left < len(nums) and right < len(nums):
+            # print(nums[left:right])
+            # print(nums[right], counts)
+            keys = counts.keys()
+            if keys:
+                M = max(*keys, nums[right])
+                m = min(*keys, nums[right])
+            else:
+                M = m = nums[right]
 
-                # reset the subarray
-                left = right
-                curMax = curMin = x
-                while abs(nums[right] - nums[left - 1]) <= 2:
-                    left -= 1
-                    curMax = max(curMax, nums[left])
-                    curMin = min(curMin, nums[left])
+            if M - m <= 2:
+                # accept it into subarray
+                counts[nums[right]] += 1
+                right += 1
+                continue
+            
+            # process the subarray
+            total += (right - left)
 
-                # minus back to avoid double-count
-                total -= (right - left + 1) * (right - left) // 2
-            # print(total)
-
-        # print(nums[left:], curMax, curMin)
+            # pop out the left item
+            if counts[nums[left]] == 1:
+                del counts[nums[left]]
+            else:
+                counts[nums[left]] -= 1
+            left += 1
 
         # process the subarray
-        total += (n - left + 1) * (n - left) // 2
+        size = right - left
+        total += (size + 1) * size // 2
 
         return total
 
