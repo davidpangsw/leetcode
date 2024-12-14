b7348d6ff3e7287b53fe9bc943be57747b44ef1e
davidpangsw <davidpangsw@hotmail.com>
2024-12-14 09:53:06 -0800

    Time: 724 ms (45.18%), Space: 27.9 MB (17.48%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.py
+++ 2762-continuous-subarrays/2762-continuous-subarrays.py
@@ -1,38 +1,40 @@
 class Solution:
     def continuousSubarrays(self, nums: List[int]) -> int:
-        left  = right = 0
+        left = right = 0
 
         total = 0
-        counts = defaultdict(int)
+        incQ = deque()
+        decQ = deque()
         while left < len(nums) and right < len(nums):
             # print(nums[left:right])
-            # print(nums[right], counts)
-            keys = counts.keys()
-            if keys:
-                M = max(*keys, nums[right])
-                m = min(*keys, nums[right])
-            else:
-                M = m = nums[right]
-
-            if M - m <= 2:
-                # accept it into subarray
-                counts[nums[right]] += 1
+            while incQ and nums[incQ[-1]] > nums[right]:
+                incQ.pop()
+            if not incQ or incQ[-1] != right:
+                incQ.append(right)
+            
+            while decQ and nums[decQ[-1]] < nums[right]:
+                decQ.pop()
+            if not decQ or decQ[-1] != right:
+                decQ.append(right)
+            
+            if nums[decQ[0]] - nums[incQ[0]] <= 2:
                 right += 1
                 continue
             
             # process the subarray
+            # print(right - left, incQ, decQ)
             total += (right - left)
 
             # pop out the left item
-            if counts[nums[left]] == 1:
-                del counts[nums[left]]
-            else:
-                counts[nums[left]] -= 1
+            if left == incQ[0]:
+                incQ.popleft()
+            if left == decQ[0]:
+                decQ.popleft()
+
             left += 1
 
         # process the subarray
-        size = right - left
-        total += (size + 1) * size // 2
+        total += (right - left + 1) * (right - left) // 2
 
         return total
 
