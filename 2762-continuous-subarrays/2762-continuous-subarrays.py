7e1800df2c24673699196ce75038b9e6aab3bde1
davidpangsw <davidpangsw@hotmail.com>
2024-12-13 21:00:22 -0800

    Time: 920 ms (18.37%), Space: 27.6 MB (28.65%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.py
+++ 2762-continuous-subarrays/2762-continuous-subarrays.py
@@ -1,26 +1,24 @@
 class Solution:
     def continuousSubarrays(self, nums: List[int]) -> int:
-        left  = 0
-        right = 0
+        left  = right = 0
 
         total = 0
         counts = defaultdict(int)
-        while left < len(nums):
+        while left < len(nums) and right < len(nums):
             # print(nums[left:right])
-            if right < len(nums):
-                # print(nums[right], counts)
-                keys = counts.keys()
-                if keys:
-                    M = max(*keys, nums[right])
-                    m = min(*keys, nums[right])
-                else:
-                    M = m = nums[right]
+            # print(nums[right], counts)
+            keys = counts.keys()
+            if keys:
+                M = max(*keys, nums[right])
+                m = min(*keys, nums[right])
+            else:
+                M = m = nums[right]
 
-                if M - m <= 2:
-                    # accept it into subarray
-                    counts[nums[right]] += 1
-                    right += 1
-                    continue
+            if M - m <= 2:
+                # accept it into subarray
+                counts[nums[right]] += 1
+                right += 1
+                continue
             
             # process the subarray
             total += (right - left)
@@ -31,6 +29,9 @@ class Solution:
             else:
                 counts[nums[left]] -= 1
             left += 1
+        # process the subarray
+        size = right - left
+        total += (size + 1) * size // 2
 
         return total
 
