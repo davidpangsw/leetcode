c248eecebe3ebd9fc50cc16b27b56829f13135f2
davidpangsw <davidpangsw@hotmail.com>
2024-12-14 22:24:40 -0800

    Time: 1060 ms (66%), Space: 54.3 MB (41.36%) - LeetHub

--- 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.py
+++ 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.py
@@ -4,15 +4,15 @@ class Solution:
         # d * n(n+1) = (pn + n) - (pn + p) = n - p
         # d = (n-p) / (n * (n+1))
 
-        arr = [(-(n-p) / (n * (n+1)), p, n) for p, n in classes]
+        arr = [(p/n - (p+1)/(n+1), p, n) for p, n in classes]
         # arr = [(-((p+1) / (n+1) - p / n), p, n) for p, n in classes]
         heapify(arr)
         for _ in range(extraStudents):
             _, p, n = heappop(arr)
             # print(p, n)
-            p, n = p+1, n+1
-            heappush(arr, (-(n-p) / (n * (n+1)), p, n))
-            # heappush(arr, (-((p+1) / (n+1) - p / n), p, n), p, n))
+            # p, n = p+1, n+1
+            # heappush(arr, (-(n-p) / (n * (n+1)), p, n))
+            heappush(arr, ((p+1)/(n+1) - (p+2)/(n+2), p+1, n+1))
         # print(arr)
         return sum([p / n for _, p, n in arr]) / len(arr)
 
