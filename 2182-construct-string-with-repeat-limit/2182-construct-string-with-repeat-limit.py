d81de57be0f77b7a4ef485879f454a6e19ad2019
davidpangsw <davidpangsw@hotmail.com>
2024-12-17 08:07:48 -0800

    Time: 140 ms (95.29%), Space: 19 MB (40.21%) - LeetHub

--- 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
@@ -1,40 +1,33 @@
 class Solution:
     def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
-        counts = [0] * 26
-        for x in s:
-            counts[ord(x) - ord('a')] += 1
-        
-        stack = []
-        for i, count in enumerate(counts):
-            if count > 0:
-                stack.append(i)
+        counts = Counter(s)
+        counts = sorted(counts.items())
+        # print(counts)
 
-        
         result = ""
-        while stack:
-            x = stack.pop()
-            if not counts[x]:
-                continue
-            
-            k = min(repeatLimit, counts[x])
-            result += chr(ord('a') + x) * k
-            counts[x] -= k
+        while counts:
+            x, count = counts.pop()
+            # print(x, count, result, x * count, result + x*count)
+            if count <= repeatLimit:
+                result += x * count
+            else:
+                result += x * repeatLimit
 
-            if counts[x]:
                 # if no backup, done
-                if not stack:
+                if not counts:
                     break
                 
                 # add one character of backup
-                result += chr(ord('a') + stack[-1])
-                counts[stack[-1]] -= 1
+                backup, backupCount = counts[-1]
+                result += backup
+                counts[-1] = (backup, backupCount - 1)
 
-                # pop out the backup if no more left
-                if not counts[stack[-1]]:
-                    stack.pop()
+                # pop out the backup if used up
+                if not counts[-1][1]:
+                    counts.pop()
                 
                 # append back the character
-                stack.append(x)
+                counts.append((x, count - repeatLimit))
             # print(result)
         return result
                 
