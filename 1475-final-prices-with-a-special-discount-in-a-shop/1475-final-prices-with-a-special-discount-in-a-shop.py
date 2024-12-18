7100f6b3454ddae06e134ddab7020421faf818c8
davidpangsw <davidpangsw@hotmail.com>
2024-12-18 00:17:36 -0800

    Time: 2 ms (63.12%), Space: 17.7 MB (10.22%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -2,12 +2,11 @@ class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
         # A "stack" by reusing prices
         # keep the stack strictly increasing
-        st = [0]
-        for i in range(1, len(prices)):
+        st = []
+        for i in range(len(prices)):
             while st and prices[i] <= prices[st[-1]]:
                 top = st.pop()
                 prices[top] -= prices[i]
             st.append(i)
         return prices
-
 
