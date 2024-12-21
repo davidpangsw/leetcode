65b476ee99bcaebac131a70b4480d93d3b1b938c
davidpangsw <davidpangsw@hotmail.com>
2024-12-20 22:26:59 -0800

    Time: 98 ms (96.34%), Space: 38.8 MB (24.39%) - LeetHub

--- 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.py
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.py
@@ -1,6 +1,32 @@
 class Solution:
     def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
         # group up the edges
+        adjLists = [[] for _ in range(n)]
+        for u, v in edges:
+            adjLists[u].append(v)
+            adjLists[v].append(u)
+        
+        result = 0
+
+        # dfs starting from any point
+        def dfs(u, fromNode):
+            for v in adjLists[u]:
+                if v == fromNode:
+                    continue
+                values[u] = (values[u] + dfs(v, u)) % k
+
+            nonlocal result
+            if values[u] % k == 0:
+                result += 1
+
+            return values[u]
+        dfs(0, -1)
+        return result
+                
+
+
+    def maxKDivisibleComponentsBFS(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
+        # group up the edges
         adjLists = [set() for _ in range(n)]
         for u, v in edges:
             adjLists[u].add(v)
@@ -10,8 +36,8 @@ class Solution:
         q = [u for u in range(n) if len(adjLists[u]) == 1]
         q = deque(q)
 
-        # BFS
-        result = 1
+        # BFS from leaves (Toposort / Kahn's algorithm)
+        result = 1 # last node must be a component itself
         while len(q) > 1:
             # print(q)
             leaf = q.popleft()
