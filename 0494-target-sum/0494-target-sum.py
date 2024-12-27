aff420efa6daea09f01dc5e4fb5348b8e5094021
davidpangsw <davidpangsw@hotmail.com>
2024-12-26 16:54:55 -0800

    Time: 120 ms (76.45%), Space: 23.2 MB (56.19%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -5,15 +5,14 @@ class Solution:
         # Recursive relation
         # f(nums[:k], target) = f(nums[:k-1], target+nums[k-1]) + f(nums[:k-1], target-nums[k-1])
         # 2^20 = 1048576 acceptable
-        n = len(nums)
-        mem = [[None for _ in range(1001)] for _ in range(n)]
+        mem = [[None for _ in range(1001)] for _ in nums]
         def f(k, t):
             # print(f"f({k},{t})")
             if k == 0:
                 return 1 if t == 0 else 0
             if t < 0:
                 return f(k, -t)
-            if t > 1000:
+            if t > 1000: # number of combs for -t and t must be the same
                 return 0
 
             i, j = k-1, t
@@ -22,5 +21,5 @@ class Solution:
             mem[i][j] = f(k-1, t+nums[k-1]) + f(k-1, t-nums[k-1])
             # print(f"f({k},{t}) = {mem[i][j]}")
             return mem[i][j]
-        return f(n, target)
+        return f(len(nums), target)
 
