095cbfd1f94f94cf2466b98461fb0b9b0e18e1df
davidpangsw <davidpangsw@hotmail.com>
2024-12-26 16:57:40 -0800

    Time: 99 ms (83.28%), Space: 21 MB (57.58%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,18 +1,19 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         nums.sort()
+        T = sum(nums)
 
         # Recursive relation
         # f(nums[:k], target) = f(nums[:k-1], target+nums[k-1]) + f(nums[:k-1], target-nums[k-1])
         # 2^20 = 1048576 acceptable
-        mem = [[None for _ in range(1001)] for _ in nums]
+        mem = [[None for _ in range(T+1)] for _ in nums]
         def f(k, t):
             # print(f"f({k},{t})")
             if k == 0:
                 return 1 if t == 0 else 0
-            if t < 0:
+            if t < 0: # number of combs for -t and t must be the same
                 return f(k, -t)
-            if t > 1000: # number of combs for -t and t must be the same
+            if t > T:
                 return 0
 
             i, j = k-1, t
