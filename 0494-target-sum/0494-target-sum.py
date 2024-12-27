30befb231dc1ca85eb999e2ed70d17f27de4a0d7
davidpangsw <davidpangsw@hotmail.com>
2024-12-26 16:50:01 -0800

    Time: 229 ms (10.75%), Space: 27.7 MB (54.06%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,5 +1,28 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        nums.sort()
+
+        # Recursive relation
+        # f(nums[:k], target) = f(nums[:k-1], target+nums[k-1]) + f(nums[:k-1], target-nums[k-1])
+        # 2^20 = 1048576 acceptable
+        n = len(nums)
+        mem = [[None for _ in range(2001)] for _ in range(n)]
+        def f(k, t):
+            # print(f"f({k},{t})")
+            if k == 0:
+                return 1 if t == 0 else 0
+            if t < -1000 or t > 1000:
+                return 0
+            i, j = k-1, t+1000
+            if mem[i][j] is not None:
+                return mem[i][j]
+            mem[i][j] = f(k-1, t+nums[k-1]) + f(k-1, t-nums[k-1])
+            # print(f"f({k},{t}) = {mem[i][j]}")
+            return mem[i][j]
+        return f(n, target)
+
+
+    def findTargetSumWaysSlow(self, nums: List[int], target: int) -> int:
         counts = Counter(nums)
         keys = sorted(counts)
         # print([(x, counts[x]) for x in keys])
@@ -42,9 +65,7 @@ class Solution:
             k = counts[x]
             result = 0
             for r in range(k+1):
-                value = (-k + r*2) * x
-                combs = C(k, r)
-                result += combs * backtracking(ind+1, t - value)
+                result += C(k, r) * backtracking(ind+1, t - (-k + r*2) * x)
             mem[i][j] = result
             # print(f"backtrack({ind},{t}) = {mem[i][j]}")
             return result
