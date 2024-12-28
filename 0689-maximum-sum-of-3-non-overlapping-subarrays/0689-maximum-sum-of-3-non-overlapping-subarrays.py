7cfa676a357dbadee766d06da9f6b376a644832d
davidpangsw <davidpangsw@hotmail.com>
2024-12-27 21:39:15 -0800

    Time: 27 ms (65.56%), Space: 22.3 MB (39.72%) - LeetHub

--- 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.py
+++ 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.py
@@ -1,31 +1,26 @@
 class Solution:
     def maxSumOfThreeSubarrays(self, nums: List[int], k: int) -> List[int]:
-        
         arr = [sum(nums[:k])]
         for i in range(1, len(nums)-k+1):
             arr.append(arr[-1] - nums[i-1] + nums[i-1+k])
         # print(arr)
-        nums = arr
-        n = len(nums)
+        n = len(arr)
         
         # find max non-k-neighbouring 1-sum in arr
-        prefixMax = [(nums[0], 0)] + [None] * (n-1)
+        prefixMax = [0] * n
         for i in range(1, n):
-            s, j = prefixMax[i-1]
-            if nums[i] > s:
-                prefixMax[i] = nums[i], i
-            else:
-                prefixMax[i] = s, j
+            j = prefixMax[i-1]
+            prefixMax[i] = i if arr[i] > arr[j] else j
         # print(prefixMax)
 
         # find max non-k-neighbouring 2-sum in arr
         prefixMax2 = [(-1, -1, -1)] * n
         for i in range(k, n):
-            a = nums[i]
-            b, j = prefixMax[i-k]
+            a = arr[i]
+            j = prefixMax[i-k]
             prev = prefixMax2[i-1]
-            if a+b > prev[0]:
-                prefixMax2[i] = a+b, j, i
+            if arr[i] + arr[j] > prev[0]:
+                prefixMax2[i] = arr[i] + arr[j], j, i
             else:
                 prefixMax2[i] = prev
         # print(prefixMax2)
@@ -33,7 +28,7 @@ class Solution:
         # find max non-k-neighbouring 3-sum in arr
         result = (-1, [-1, -1, -1])
         for i in range(k, n):
-            a = nums[i]
+            a = arr[i]
             b, x, y = prefixMax2[i-k]
             if a + b > result[0]:
                 result = a+b, [x, y, i]
