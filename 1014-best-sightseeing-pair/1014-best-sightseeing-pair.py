949abe4e038d81a7873a4427c2c7f5b5b9f5d116
davidpangsw <davidpangsw@hotmail.com>
2024-12-27 22:59:36 -0800

    Time: 62 ms (78.04%), Space: 23.2 MB (21.48%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
@@ -1,11 +1,9 @@
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
-        st = [values[0] + 0] # decreasing
         n = len(values)
+        curMax = values[0] + 0
         result = -1
         for j in range(1, n):
-            result = max(result, values[j] - j + st[0])
-            while st and st[-1] < values[j] + j:
-                st.pop()
-            st.append(values[j] + j)
+            result = max(result, values[j] - j + curMax)
+            curMax = max(curMax, values[j] + j)
         return result
