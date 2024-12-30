d6a1fc8ddac69cd1b63b5c2a67660015aa8df79e
davidpangsw <davidpangsw@hotmail.com>
2024-12-30 08:49:13 -0800

    Time: 85 ms (83.07%), Space: 22.3 MB (66.14%) - LeetHub

---
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
@@ -0,0 +1,18 @@
+M = 10**9+7
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        # f(s) = #different good strings of size = s
+        # f(s) = f(s-zero) + f(s-one)
+        # f(0) = 1
+        # f(s) = 0 for s < 0
+        # result = f(high) - f(low-1)
+
+        mem = [0] * (high + 1)
+        mem[0] = 1
+        for s in range(1, high+1):
+            if s >= zero:
+                mem[s] += mem[s-zero] % M
+            if s >= one:
+                mem[s] += mem[s-one] % M
+        # print(mem)
+        return sum(mem[low:high+1]) % M
