17b8722c2c7ccb476e97466b7b659deecb8100e9
davidpangsw <davidpangsw@hotmail.com>
2025-01-05 22:46:00 -0800

    Time: 1431 ms (35.19%), Space: 18 MB (33.8%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,10 +1,13 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
+        ones = []
         n = len(boxes)
         results = [0] * n
         for i in range(n):
             if boxes[i] == "1":
-                for j in range(n):
-                    results[j] += abs(j-i)
+                ones.append(i)
+        for i in range(n):
+            for j in ones:
+                results[i] += abs(j-i)
             # print(results)
         return results
