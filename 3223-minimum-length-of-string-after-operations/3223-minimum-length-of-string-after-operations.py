581b29b4fea66f9353cd86419da653819c579cd5
davidpangsw <davidpangsw@hotmail.com>
2025-01-13 15:34:28 -0800

    Time: 69 ms (79.67%), Space: 18.6 MB (63.93%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.py
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.py
@@ -1,15 +1,12 @@
-maps = [1, 2, 1]
-changes = [1, 1, -1]
 class Solution:
+
     def minimumLength(self, s: str) -> int:
-        # 0: 0
-        # odd: 1
-        # even: 2
-        # 0 -> 1 -> 2 -> 1 -> ...
-        result = 0
-        counts = defaultdict(int)
-        for x in s:
-            c = counts[x]
-            result += changes[c]
-            counts[x] = maps[c]
-        return result
+        s_set = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+        ans = 0
+        for ch in s_set:
+            count = s.count(ch)
+            if count & 1:
+                ans += 1
+            elif count != 0:
+                ans += 2
+        return ans
