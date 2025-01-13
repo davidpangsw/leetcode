c0d3653f865ca210224e3d29113f808f581add8f
davidpangsw <davidpangsw@hotmail.com>
2025-01-13 15:22:05 -0800

    Time: 146 ms (40.33%), Space: 19 MB (15.41%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.py
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.py
@@ -1,8 +1,3 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
-        # 1 -> 1
-        # 2 -> 2
-        # 3 -> 1
-        # 4 -> 2
-        # 5 -> 1
-        return sum([1 if x & 1 == 1 else 2 for x in Counter(s).values()])
+        return sum([2 - (x & 1) for x in Counter(s).values()])
